ee06d870e5a048db03c103a9f99aeccd12a5f01e
Adam Spiers <adam@pantherprotocol.io>
2021-08-05 12:06:27 +0100

    Remove unneeded hash functions

--- panther-protocol-contracts-ws/contracts/Poseidon.sol
+++ panther-protocol-contracts-ws/contracts/Poseidon.sol
@@ -4,8 +4,3 @@ pragma solidity ^0.6.12;
 library PoseidonT3 {
     function poseidon(uint256[2] memory input) public pure returns (uint256) {}
 }
-
-
-library PoseidonT6 {
-    function poseidon(uint256[5] memory input) public pure returns (uint256) {}
-}

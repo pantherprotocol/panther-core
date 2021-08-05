6b537dcbfd240aa8ad0d227a66db0f0d21ab0525
Adam Spiers <adam@pantherprotocol.io>
2021-08-05 11:59:17 +0100

    Import Merkle tree Solidity from MACI
    https://github.com/appliedzkp/maci

---
+++ panther-protocol-contracts-ws/scripts/buildPoseidon.ts
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+import * as Artifactor from 'truffle-artifactor'
+
+const poseidonGenContract = require('circomlib/src/poseidon_gencontract.js')
+const artifactor = new Artifactor('compiled/')
+
+const buildPoseidonT3 = async () => {
+  await artifactor.save({
+    contractName: 'PoseidonT3',
+    abi: poseidonGenContract.generateABI(2),
+    unlinked_binary: poseidonGenContract.createCode(2)
+  })
+}
+
+const buildPoseidonT6 = async () => {
+  await artifactor.save({
+    contractName: 'PoseidonT6',
+    abi: poseidonGenContract.generateABI(5),
+    unlinked_binary: poseidonGenContract.createCode(5)
+  })
+}
+
+if (require.main === module) {
+  buildPoseidonT3()
+  buildPoseidonT6()
+}
+
+export { buildPoseidonT3, buildPoseidonT6 }

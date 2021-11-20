db11539e62c1b54d408926e6c926cac7fcc64ad7
pycckuu <is.markelov@gmail.com>
2021-11-20 17:31:46 +0100

    ✨ (TMT): Generation, saving, loading and compression

--- src/triad-merkle-tree/__tests__/service.test.ts
+++ src/triad-merkle-tree/__tests__/service.test.ts
@@ -1,21 +1,50 @@
 import { describe, expect } from '@jest/globals';
 
 import Service from '../service'
+import { TriadMerkleTree } from '../../triad-merkle-tree';
 import { fourthTree } from './data/trees';
+import fs from 'fs';
 
-describe('Generation of the Triad Merkle tree', () => {
-    it('should generate correct tree from 1536 commitments of 32 bytes hashes', () => {
+describe('Generation, loading and compression of the Triad Merkle tree', () => {
+    let tree: TriadMerkleTree;
+    beforeAll(() => {
         const commitments = new Array<string>(1536);
         for (let i = 0; i < 1536; i++) {
             commitments[i] = '0x' + BigInt(i).toString(16);
         }
 
-        const tree = Service.createTriadMerkleTree(commitments);
+        tree = Service.createTriadMerkleTree(commitments);
+    })
+
+    it('should generate correct tree from 1536 commitments of 32 bytes hashes', () => {
         expect('0x' + BigInt(tree.root).toString(16)).toBe(fourthTree.root);
     })
 
+    it('shoud be saved and loaded without compression', () => {
+        const w = Service.stringifyTree(tree, false);
+        fs.writeFileSync('src/triad-merkle-tree/__tests__/data/tree.txt', w, 'ucs2');
 
+        const r = fs.readFileSync('src/triad-merkle-tree/__tests__/data/tree.txt', 'ucs2');
+        const t = Service.loadTree(r, false);
 
+        expect('0x' + BigInt(t.root).toString(16)).toBe(fourthTree.root);
+    })
 
+    it('shoud be saved and loaded with compression', () => {
+        const w = Service.stringifyTree(tree, true);
+        fs.writeFileSync('src/triad-merkle-tree/__tests__/data/tree_zip.txt', w, 'ucs2');
+
+        const r = fs.readFileSync('src/triad-merkle-tree/__tests__/data/tree_zip.txt', 'ucs2');
+        const t = Service.loadTree(r, true);
+
+        expect('0x' + BigInt(t.root).toString(16)).toBe(fourthTree.root);
+    })
+
+    it('compression and decompression should give the same result', () => {
+        const w = tree.serialize();
+        const c = Service.compressString(w);
+        const r = Service.decompressString(c);
+        expect(r).toBe(w);
+    })
 
 })
--- src/triad-merkle-tree/service.ts
+++ src/triad-merkle-tree/service.ts
@@ -1,11 +1,13 @@
 import { TriadMerkleTree, hash23 } from "../triad-merkle-tree"
 
+import LZString from "lz-string"
 import _ from "lodash"
 import assert from "assert"
 
 const LEAF_NODE_SIZE = 3;
 const TREE_SIZE = 1536
 
+// returns a new triad merkle tree constructed of passed 1536! commitments
 const createTriadMerkleTree = (commitments: string[]): TriadMerkleTree => {
     assert(commitments.length === TREE_SIZE, "Commitments must be of length 1536")
 
@@ -18,4 +20,27 @@ const createTriadMerkleTree = (commitments: string[]): TriadMerkleTree => {
     return triadMerkleTree
 };
 
-export default { createTriadMerkleTree }
+// compress string before storing it
+const compressString = (s: string): string => {
+    return LZString.compressToUTF16(s);
+}
+
+const decompressString = (s: string): string | null => {
+    return LZString.decompressFromUTF16(s);
+}
+
+// produces a string that can be stored on local drive
+const stringifyTree = (tree: TriadMerkleTree, compression: boolean): string => {
+    return compression ? compressString(tree.serialize()) : tree.serialize();
+}
+
+// loads triad merkle tree from string (compressed or not)
+const loadTree = (s: string, compression: boolean): TriadMerkleTree => {
+    const treeString = compression ? decompressString(s) : s;
+    if (treeString === null) {
+        throw new Error("Could not decompress tree string")
+    }
+    return TriadMerkleTree.deserialize(treeString);
+}
+
+export default { createTriadMerkleTree, stringifyTree, loadTree, compressString, decompressString }

76a53e9572280a5e35cb6adfc7fc16efecba6a1f
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2021-12-29 04:41:25 +0100

    Adding initial dapp web3 connector structure

---
+++ dapp/src/components/NavigationButton/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './styles.scss';
+
+export const NavigationBtn = props => {
+    return (
+        <div className="select-navi-holder">
+            <button className="select-navi">
+                <div className="currency-navi-holder">
+                    <img
+                        className="currency-logo"
+                        src={props.image}
+                        alt="Crytocurrency Logo"
+                    />
+                    <div className="navi-btn-text-hold">
+                        <p className="front-text"> {props.balance}</p>
+                        <p className="main-text">{props.address}</p>
+                    </div>
+                </div>
+            </button>
+        </div>
+    );
+};

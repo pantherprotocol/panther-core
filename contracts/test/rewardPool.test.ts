1380c4afe2e175641e16eff897d67b1d0161451f
Adam Spiers <adam@pantherprotocol.io>
2022-01-07 16:39:16 +0000

    contracts: enhance rewardPool.test.ts
    - Wrap all tests in outer describe block
    - Reuse before / beforeEach hooks
    - Avoid implicit 'any' types
    - Switch from expectRevert to waffle expect(...).to.be.revertedWith(...)

--- contracts/test/rewardPool.test.ts
+++ contracts/test/rewardPool.test.ts
@@ -1,6 +1,5 @@
 import chai from 'chai';
 const expect = chai.expect;
-import {expectRevert} from '@openzeppelin/test-helpers';
 import hre from 'hardhat';
 const {getSigners} = hre.ethers;
 import {BigNumber, BaseContract} from 'ethers';
@@ -14,145 +13,11 @@ function toBN(n: number) {
     return BigNumber.from(n);
 }
 
-let rewardPool: MockContract<BaseContract>;
-let vestingPoolFactory: FakeContract<BaseContract>;
-let deployer: SignerWithAddress;
-let alice: SignerWithAddress;
-
-describe('RewardPool Contract initialisation', async () => {
-    before(async function () {
-        [deployer, alice] = await getSigners();
-    });
-    beforeEach(async () => {
-        const rewardPoolFactory = await smock.mock('RewardPool');
-        vestingPoolFactory = await smock.fake('IVestingPools');
-        rewardPool = await rewardPoolFactory.deploy(
-            vestingPoolFactory.address,
-            deployer.address,
-        );
-    });
-
-    it('return error in initialisation when expired timestamp is passed', async () => {
-        // const f = await rewardPool.transferPoolWalletRole(wallet1.address)
-        // vestingPoolFactory.getWallet.returns(rewardPool.address)
-        const expiryTime = Math.round(+new Date() / 1000) - 100;
-        await expectRevert(
-            rewardPool.initialize(0, alice.address, expiryTime),
-            "VM Exception while processing transaction: reverted with reason string 'RP: expired'",
-        );
-    });
-
-    it('return error in initialisation when 0th address is not used', async () => {
-        const expiryTime = Math.round(+new Date() / 1000) - 100;
-        await expectRevert(
-            rewardPool.initialize(
-                0,
-                '0x0000000000000000000000000000000000000000',
-                expiryTime,
-            ),
-            "VM Exception while processing transaction: reverted with reason string 'RP: zero address'",
-        );
-    });
-
-    it('return error in initialisation when wallet address of pool is not used', async () => {
-        const expiryTime = Math.round(+new Date() / 1000) + 100;
-        await expectRevert(
-            rewardPool.initialize(0, alice.address, expiryTime),
-            "VM Exception while processing transaction: reverted with reason string 'RP:E7'",
-        );
-    });
-
-    it('successful initialization of the contract', async () => {
-        vestingPoolFactory.getWallet.returns(rewardPool.address);
-        const expiryTime = Math.round(+new Date() / 1000) + 100;
-        await expect(rewardPool.initialize(0, alice.address, expiryTime))
-            .to.emit(rewardPool, 'Initialized')
-            .withArgs(0, alice.address, expiryTime);
-    });
-});
-
-describe('RewardPool releasable amount', async () => {
-    let rewardPool;
-    let vestingPoolFactory;
-    before(async function () {
-        [deployer, alice] = await getSigners();
-    });
-    beforeEach(async () => {
-        const rewardPoolFactory = await smock.mock('RewardPool');
-        vestingPoolFactory = await smock.fake('IVestingPools');
-        rewardPool = await rewardPoolFactory.deploy(
-            vestingPoolFactory.address,
-            deployer.address,
-        );
-    });
-
-    it('return 0 releasable amount of tokens when tokens are not vested', async () => {
-        vestingPoolFactory.getWallet.returns(rewardPool.address);
-        const expiryTime = Math.round(+new Date() / 1000) + 100;
-        await rewardPool.initialize(0, alice.address, expiryTime);
-        const releasableAmount = await rewardPool.releasableAmount();
-        expect(releasableAmount).to.eql(toBN(0));
-    });
-
-    it('return 100 releasable amount of tokens when 100 tokens are vested', async () => {
-        vestingPoolFactory.getWallet.returns(rewardPool.address);
-        vestingPoolFactory.releasableAmount.returns(100);
-        const expiryTime = Math.round(+new Date() / 1000) + 100;
-        await rewardPool.initialize(0, alice.address, expiryTime);
-        const releasableAmount = await rewardPool.releasableAmount();
-        expect(releasableAmount).to.eql(toBN(100));
-    });
-});
-
-describe('RewardPool vest rewards', async () => {
-    let rewardPool;
-    let vestingPoolFactory;
-    before(async function () {
-        [deployer, alice] = await getSigners();
-    });
-    beforeEach(async () => {
-        const rewardPoolFactory = await smock.mock('RewardPool');
-        vestingPoolFactory = await smock.fake('IVestingPools');
-        rewardPool = await rewardPoolFactory.deploy(
-            vestingPoolFactory.address,
-            deployer.address,
-        );
-    });
-    it('revert if unauthorized vestRewards() call is made', async () => {
-        vestingPoolFactory.getWallet.returns(rewardPool.address);
-        vestingPoolFactory.releasableAmount.returns(100);
-        const expiryTime = Math.round(+new Date() / 1000) + 100;
-        await rewardPool.initialize(0, alice.address, expiryTime);
-        await expectRevert(
-            rewardPool.vestRewards(),
-            "VM Exception while processing transaction: reverted with reason string 'RP: unauthorized'",
-        );
-    });
-
-    it('doesnt release any reward tokens to recipient when releasable amount is 0', async () => {
-        vestingPoolFactory.getWallet.returns(rewardPool.address);
-        vestingPoolFactory.releasableAmount.returns(0);
-        const expiryTime = Math.round(+new Date() / 1000) + 100;
-        await rewardPool.initialize(0, alice.address, expiryTime);
-        await expect(rewardPool.connect(alice).vestRewards()).not.to.emit(
-            rewardPool,
-            'Vested',
-        );
-    });
-
-    it('successfully vest and release 100 reward tokens to recipient', async () => {
-        vestingPoolFactory.getWallet.returns(rewardPool.address);
-        vestingPoolFactory.releasableAmount.returns(100);
-        const expiryTime = Math.round(+new Date() / 1000) + 100;
-        await rewardPool.initialize(0, alice.address, expiryTime);
-        await expect(rewardPool.connect(alice).vestRewards()).to.emit(
-            rewardPool,
-            'Vested',
-        );
-    });
-});
-
-describe('RewardPool transfer wallet role', async () => {
+describe('RewardPool', async () => {
+    let rewardPool: MockContract<BaseContract>;
+    let vestingPoolFactory: FakeContract<BaseContract>;
+    let deployer: SignerWithAddress;
+    let alice: SignerWithAddress;
     let wallet1: SignerWithAddress;
     let wallet2: SignerWithAddress;
 
@@ -168,37 +33,165 @@ describe('RewardPool transfer wallet role', async () => {
         );
     });
 
-    it('revert when 0 address passed while transferring wallet role', async () => {
-        vestingPoolFactory.getWallet.returns(rewardPool.address);
-        vestingPoolFactory.releasableAmount.returns(100);
-        const expiryTime = Math.round(+new Date() / 1000) + 100;
-        await rewardPool.initialize(0, alice.address, expiryTime);
-        await expectRevert(
-            rewardPool.transferPoolWalletRole(
-                '0x0000000000000000000000000000000000000000',
-            ),
-            "VM Exception while processing transaction: reverted with reason string 'RP: zero address",
-        );
+    describe('contract initialisation', async () => {
+        it('returns error in initialisation when expired timestamp is passed', async () => {
+            // const f = await rewardPool.transferPoolWalletRole(wallet1.address)
+            // vestingPoolFactory.getWallet.returns(rewardPool.address)
+            const expiryTime = Math.round(+new Date() / 1000) - 100;
+            await expect(
+                rewardPool.initialize(0, alice.address, expiryTime),
+            ).to.be.revertedWith(
+                "VM Exception while processing transaction: reverted with reason string 'RP: expired'",
+            );
+        });
+
+        it('returns error in initialisation when 0th address is not used', async () => {
+            const expiryTime = Math.round(+new Date() / 1000) - 100;
+            await expect(
+                rewardPool.initialize(
+                    0,
+                    '0x0000000000000000000000000000000000000000',
+                    expiryTime,
+                ),
+            ).to.be.revertedWith(
+                "VM Exception while processing transaction: reverted with reason string 'RP: zero address'",
+            );
+        });
+
+        it('returns error in initialisation when wallet address of pool is not used', async () => {
+            const expiryTime = Math.round(+new Date() / 1000) + 100;
+            await expect(
+                rewardPool.initialize(0, alice.address, expiryTime),
+            ).to.be.revertedWith(
+                "VM Exception while processing transaction: reverted with reason string 'RP:E7'",
+            );
+        });
+
+        it('successfully initializes the contract', async () => {
+            vestingPoolFactory.getWallet.returns(rewardPool.address);
+            const expiryTime = Math.round(+new Date() / 1000) + 100;
+            await expect(rewardPool.initialize(0, alice.address, expiryTime))
+                .to.emit(rewardPool, 'Initialized')
+                .withArgs(0, alice.address, expiryTime);
+        });
     });
 
-    it('revert when non owner calls transferPoolWalletRole()', async () => {
-        vestingPoolFactory.getWallet.returns(rewardPool.address);
-        vestingPoolFactory.releasableAmount.returns(100);
-        const expiryTime = Math.round(+new Date() / 1000) + 100;
-        await rewardPool.initialize(0, alice.address, expiryTime);
-        await expectRevert(
-            rewardPool.connect(wallet1).transferPoolWalletRole(wallet2.address),
-            "VM Exception while processing transaction: reverted with reason string 'ImmOwn: unauthorized",
-        );
+    describe('releasable amount', async () => {
+        beforeEach(async () => {
+            const rewardPoolFactory = await smock.mock('RewardPool');
+            vestingPoolFactory = await smock.fake('IVestingPools');
+            rewardPool = await rewardPoolFactory.deploy(
+                vestingPoolFactory.address,
+                deployer.address,
+            );
+        });
+
+        it('return 0 releasable amount of tokens when tokens are not vested', async () => {
+            vestingPoolFactory.getWallet.returns(rewardPool.address);
+            const expiryTime = Math.round(+new Date() / 1000) + 100;
+            await rewardPool.initialize(0, alice.address, expiryTime);
+            const releasableAmount = await rewardPool.releasableAmount();
+            expect(releasableAmount).to.eql(toBN(0));
+        });
+
+        it('return 100 releasable amount of tokens when 100 tokens are vested', async () => {
+            vestingPoolFactory.getWallet.returns(rewardPool.address);
+            vestingPoolFactory.releasableAmount.returns(100);
+            const expiryTime = Math.round(+new Date() / 1000) + 100;
+            await rewardPool.initialize(0, alice.address, expiryTime);
+            const releasableAmount = await rewardPool.releasableAmount();
+            expect(releasableAmount).to.eql(toBN(100));
+        });
     });
 
-    it('successful transfer pool wallet address', async () => {
-        vestingPoolFactory.getWallet.returns(rewardPool.address);
-        vestingPoolFactory.releasableAmount.returns(100);
-        const expiryTime = Math.round(+new Date() / 1000) + 100;
-        await rewardPool.initialize(0, alice.address, expiryTime);
-        await rewardPool
-            .connect(deployer)
-            .transferPoolWalletRole(wallet2.address);
+    describe('RewardPool vest rewards', async () => {
+        beforeEach(async () => {
+            const rewardPoolFactory = await smock.mock('RewardPool');
+            vestingPoolFactory = await smock.fake('IVestingPools');
+            rewardPool = await rewardPoolFactory.deploy(
+                vestingPoolFactory.address,
+                deployer.address,
+            );
+        });
+        it('revert if unauthorized vestRewards() call is made', async () => {
+            vestingPoolFactory.getWallet.returns(rewardPool.address);
+            vestingPoolFactory.releasableAmount.returns(100);
+            const expiryTime = Math.round(+new Date() / 1000) + 100;
+            await rewardPool.initialize(0, alice.address, expiryTime);
+            await expect(rewardPool.vestRewards()).to.be.revertedWith(
+                "VM Exception while processing transaction: reverted with reason string 'RP: unauthorized'",
+            );
+        });
+
+        it('doesnt release any reward tokens to recipient when releasable amount is 0', async () => {
+            vestingPoolFactory.getWallet.returns(rewardPool.address);
+            vestingPoolFactory.releasableAmount.returns(0);
+            const expiryTime = Math.round(+new Date() / 1000) + 100;
+            await rewardPool.initialize(0, alice.address, expiryTime);
+            await expect(rewardPool.connect(alice).vestRewards()).not.to.emit(
+                rewardPool,
+                'Vested',
+            );
+        });
+
+        it('successfully vest and release 100 reward tokens to recipient', async () => {
+            vestingPoolFactory.getWallet.returns(rewardPool.address);
+            vestingPoolFactory.releasableAmount.returns(100);
+            const expiryTime = Math.round(+new Date() / 1000) + 100;
+            await rewardPool.initialize(0, alice.address, expiryTime);
+            await expect(rewardPool.connect(alice).vestRewards()).to.emit(
+                rewardPool,
+                'Vested',
+            );
+        });
+    });
+
+    describe('transferPoolWalletRole', async () => {
+        beforeEach(async () => {
+            const rewardPoolFactory = await smock.mock('RewardPool');
+            vestingPoolFactory = await smock.fake('IVestingPools');
+            rewardPool = await rewardPoolFactory.deploy(
+                vestingPoolFactory.address,
+                deployer.address,
+            );
+        });
+
+        it('reverts when 0 address passed while transferring wallet role', async () => {
+            vestingPoolFactory.getWallet.returns(rewardPool.address);
+            vestingPoolFactory.releasableAmount.returns(100);
+            const expiryTime = Math.round(+new Date() / 1000) + 100;
+            await rewardPool.initialize(0, alice.address, expiryTime);
+            await expect(
+                rewardPool.transferPoolWalletRole(
+                    '0x0000000000000000000000000000000000000000',
+                ),
+            ).to.be.revertedWith(
+                "VM Exception while processing transaction: reverted with reason string 'RP: zero address",
+            );
+        });
+
+        it('reverts when non-owner calls transferPoolWalletRole()', async () => {
+            vestingPoolFactory.getWallet.returns(rewardPool.address);
+            vestingPoolFactory.releasableAmount.returns(100);
+            const expiryTime = Math.round(+new Date() / 1000) + 100;
+            await rewardPool.initialize(0, alice.address, expiryTime);
+            await expect(
+                rewardPool
+                    .connect(wallet1)
+                    .transferPoolWalletRole(wallet2.address),
+            ).to.be.revertedWith(
+                "VM Exception while processing transaction: reverted with reason string 'ImmOwn: unauthorized",
+            );
+        });
+
+        it('successfully transfers pool wallet address', async () => {
+            vestingPoolFactory.getWallet.returns(rewardPool.address);
+            vestingPoolFactory.releasableAmount.returns(100);
+            const expiryTime = Math.round(+new Date() / 1000) + 100;
+            await rewardPool.initialize(0, alice.address, expiryTime);
+            await rewardPool
+                .connect(deployer)
+                .transferPoolWalletRole(wallet2.address);
+        });
     });
 });

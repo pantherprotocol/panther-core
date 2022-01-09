8b73fa0dd6d8f81316155ccea9cae01b3a907ae4
bitnician <b3hzad@me.com>
2022-01-09 19:03:49 +0330

    test: add more unit tests for creating share using `onAction()`

--- contracts/test/rewardMaster.test.ts
+++ contracts/test/rewardMaster.test.ts
@@ -11,7 +11,7 @@ import {
     IRewardAdviser,
 } from '../types/contracts';
 
-describe.only('Reward Master', () => {
+describe('Reward Master', () => {
     let rewardPool: FakeContract<RewardPool>;
     let rewardToken: FakeContract<IErc20Min>;
     let rewardAdviser: FakeContract<IRewardAdviser>;
@@ -21,6 +21,7 @@ describe.only('Reward Master', () => {
     let user_1: SignerWithAddress;
     let user_2: SignerWithAddress;
     let user_3: SignerWithAddress;
+    let user_4: SignerWithAddress;
     let startBlock: number;
     let provider: Provider;
 
@@ -31,7 +32,8 @@ describe.only('Reward Master', () => {
     };
 
     before(async () => {
-        [owner, oracle, user_1, user_2, user_3] = await ethers.getSigners();
+        [owner, oracle, user_1, user_2, user_3, user_4] =
+            await ethers.getSigners();
 
         rewardPool = await smock.fake('RewardPool');
         rewardToken = await smock.fake('IErc20Min');
@@ -53,6 +55,7 @@ describe.only('Reward Master', () => {
 
     describe('public variables', () => {
         before(async () => await deployRewardMaster());
+
         it('should get the reward pool address', async () => {
             const rewardPoolAddress = await rewardMaster.REWARD_POOL();
             expect(rewardPoolAddress).to.equal(rewardPool.address);
@@ -79,8 +82,9 @@ describe.only('Reward Master', () => {
         });
     });
 
-    describe('Function permissions', () => {
+    describe('Add/Remove advisor', () => {
         const action = ethers.utils.id('STAKE').slice(0, 10); // bytes4
+
         before(async () => await deployRewardMaster());
 
         it('should only let owner to add reward advisor ', async () => {
@@ -115,14 +119,6 @@ describe.only('Reward Master', () => {
         });
 
         it('should only let owner to remove reward advisor', async () => {
-            rewardMaster
-                .connect(owner)
-                .addRewardAdviser(
-                    oracle.address,
-                    action,
-                    rewardAdviser.address,
-                );
-
             await expect(
                 rewardMaster
                     .connect(owner)
@@ -144,11 +140,9 @@ describe.only('Reward Master', () => {
                     .removeRewardAdviser(oracle.address, action),
             ).revertedWith('ImmOwn: unauthorized');
         });
-
-        // it('should only let owner to force-withdraw any erc20 token', async () => {});
     });
 
-    describe.only('#onAction()', () => {
+    describe('#onAction()', () => {
         const action = ethers.utils.id('STAKED_OR_UNSTAKED').slice(0, 10); // bytes4
         const message = ethers.utils.id('AN_ENCODED_MESSAGE');
         const vestedRewards = BigNumber.from(10).pow(24); // 1000000e18
@@ -220,7 +214,7 @@ describe.only('Reward Master', () => {
                 expect(await rewardMaster.lastBalance()).to.be.eq(0);
             });
 
-            it('should grant shares to the second user (user_2)', async () => {
+            it('should grant shares to the second user (user_2) and vest tokens to contract', async () => {
                 stakeAdvice.createSharesFor = user_2.address;
 
                 rewardAdviser.adviceReward.returns(stakeAdvice);
@@ -241,16 +235,13 @@ describe.only('Reward Master', () => {
                 const {shares, offset} = await rewardMaster.records(
                     stakeAdvice.createSharesFor,
                 );
+
                 const accumRewardPerShare = vestedRewards
                     .mul(accumRewardPerShareScale)
                     .div(initialTotalShares);
 
                 expect(shares).to.be.eq(stakeAdvice.sharesToCreate);
-                expect(offset).to.be.eq(
-                    stakeAdvice.sharesToCreate
-                        .mul(accumRewardPerShare)
-                        .div(accumRewardPerShareScale),
-                );
+                expect(offset).to.be.eq(vestedRewards);
                 expect(await rewardMaster.accumRewardPerShare()).to.be.eq(
                     accumRewardPerShare,
                 );
@@ -265,11 +256,45 @@ describe.only('Reward Master', () => {
                 );
             });
 
-            it('should grant shares to the third user (user_3)', async () => {
-                // resets mock behavior (we do not release token for second time)
+            it('should grant shares to the third user (user_3) and do not vest token to contract', async () => {
+                // resets mock behavior (we do not release token for second time in this test case)
                 rewardPool.vestRewards.reset();
 
                 stakeAdvice.createSharesFor = user_3.address;
+
+                rewardAdviser.adviceReward.returns(stakeAdvice);
+
+                const initialTotalShares = await rewardMaster.totalShares();
+
+                await expect(
+                    rewardMaster.connect(oracle).onAction(action, message),
+                )
+                    .to.emit(rewardMaster, 'SharesGranted')
+                    .withArgs(
+                        stakeAdvice.createSharesFor,
+                        stakeAdvice.sharesToCreate,
+                    );
+
+                const {shares, offset} = await rewardMaster.records(
+                    stakeAdvice.createSharesFor,
+                );
+
+                expect(shares).to.be.eq(stakeAdvice.sharesToCreate);
+                expect(offset).to.be.eq(vestedRewards);
+
+                expect(await rewardMaster.totalShares()).to.be.eq(
+                    stakeAdvice.sharesToCreate.add(initialTotalShares),
+                );
+                expect(await rewardMaster.lastBalance()).to.be.eq(
+                    vestedRewards,
+                );
+            });
+
+            it('should grant shares to the forth user (user_4) and vest tokens to the contract', async () => {
+                rewardPool.vestRewards.returns(vestedRewards);
+                rewardToken.balanceOf.returns(vestedRewards.mul(2));
+
+                stakeAdvice.createSharesFor = user_4.address;
 
                 rewardAdviser.adviceReward.returns(stakeAdvice);
 
@@ -302,7 +327,7 @@ describe.only('Reward Master', () => {
                     stakeAdvice.sharesToCreate.add(initialTotalShares),
                 );
                 expect(await rewardMaster.lastBalance()).to.be.eq(
-                    vestedRewards,
+                    vestedRewards.mul(2),
                 );
             });
 
@@ -331,6 +356,7 @@ describe.only('Reward Master', () => {
                 expect(oldTotalShares).to.be.eq(newTotalShares);
             });
         });
+
         describe('redeem shares', () => {
             const amountToStake = BigNumber.from(20).mul(
                 BigNumber.from(10).pow(18),
@@ -351,7 +377,6 @@ describe.only('Reward Master', () => {
 
                 // release rewards to reward master contract
                 rewardPool.vestRewards.returns(vestedRewards);
-                rewardToken.balanceOf.returns(vestedRewards);
                 rewardToken.transfer.returns(true);
 
                 // add reward advisor
@@ -374,10 +399,12 @@ describe.only('Reward Master', () => {
                 };
 
                 // create shares for users
-                const users = [user_1, user_2];
-                for (const user of users) {
-                    stakedAdvice.createSharesFor = user.address;
+                const users = [user_1, user_2, user_3, user_4];
+                for (let i = 0; i < users.length; i++) {
+                    stakedAdvice.createSharesFor = users[i].address;
                     rewardAdviser.adviceReward.returns(stakedAdvice);
+
+                    rewardToken.balanceOf.returns(vestedRewards.mul(i));
 
                     await rewardMaster
                         .connect(oracle)
@@ -397,9 +424,11 @@ describe.only('Reward Master', () => {
                     await rewardMaster.accumRewardPerShare();
 
                 const totalShares = await rewardMaster.totalShares();
+                const oldLastBalance = await rewardMaster.lastBalance();
                 const {shares, offset} = await rewardMaster.records(
                     user_1.address,
                 );
+
                 const reward = shares
                     .mul(accumRewardPerShare)
                     .div(accumRewardPerShareScale)
@@ -413,23 +442,246 @@ describe.only('Reward Master', () => {
 
                 const {shares: updatedShares, offset: updatedOffset} =
                     await rewardMaster.records(user_1.address);
+                const lastBalance = await rewardMaster.lastBalance();
 
                 expect(await rewardMaster.totalShares()).to.be.eq(
                     totalShares.sub(shares),
                 );
                 expect(updatedShares).to.be.eq(BigNumber.from(0));
                 expect(updatedOffset).to.be.eq(BigNumber.from(0));
+                expect(lastBalance).to.be.eq(oldLastBalance.sub(reward));
+
+                // nobody is sent tokens to contract directly
+                rewardToken.balanceOf.returns(lastBalance);
+            });
+
+            it('should not redeem shares of the user who has not enough shares', async () => {
+                await expect(
+                    rewardMaster.connect(oracle).onAction(action, message),
+                ).to.be.revertedWith('RM: Not enough shares to redeem');
+            });
+
+            it('should redeem part of shares of the user_2 who has been granted shares', async () => {
+                unstakedAdvice.redeemSharesFrom = user_2.address;
+                unstakedAdvice.sendRewardTo = user_2.address;
+                unstakedAdvice.sharesToRedeem = unstakedAdvice.sharesToRedeem
+                    .mul(9)
+                    .div(10); // 0.9 of the shares
+
+                rewardAdviser.adviceReward.returns(unstakedAdvice);
+
+                const accumRewardPerShare =
+                    await rewardMaster.accumRewardPerShare();
+
+                const {shares, offset} = await rewardMaster.records(
+                    user_2.address,
+                );
+                const totalShares = await rewardMaster.totalShares();
+                const lastBalance = await rewardMaster.lastBalance();
+
+                const reward = unstakedAdvice.sharesToRedeem
+                    .mul(accumRewardPerShare)
+                    .div(accumRewardPerShareScale)
+                    .sub(offset);
+
+                await expect(
+                    rewardMaster.connect(oracle).onAction(action, message),
+                )
+                    .to.emit(rewardMaster, 'RewardPaid')
+                    .withArgs(user_2.address, reward);
+
+                const {shares: updatedShares, offset: updatedOffset} =
+                    await rewardMaster.records(user_2.address);
+
+                const updatedLastBalance = await rewardMaster.lastBalance();
+
+                expect(await rewardMaster.totalShares()).to.be.eq(
+                    totalShares.sub(unstakedAdvice.sharesToRedeem),
+                );
+                expect(updatedLastBalance).to.be.eq(lastBalance.sub(reward));
+                expect(updatedShares).to.be.eq(
+                    shares.sub(unstakedAdvice.sharesToRedeem),
+                );
+                expect(updatedOffset).to.be.eq(
+                    offset.mul(unstakedAdvice.sharesToRedeem).div(shares),
+                );
+
+                // nobody is sent tokens to contract directly
+                rewardToken.balanceOf.returns(updatedLastBalance);
+            });
+
+            it('should redeem the rest of shares of the user_2', async () => {
+                const {shares} = await rewardMaster.records(user_2.address);
+
+                unstakedAdvice.redeemSharesFrom = user_2.address;
+                unstakedAdvice.sendRewardTo = user_2.address;
+                unstakedAdvice.sharesToRedeem = shares;
+
+                rewardAdviser.adviceReward.returns(unstakedAdvice);
+
+                const accumRewardPerShare =
+                    await rewardMaster.accumRewardPerShare();
+
+                const totalShares = await rewardMaster.totalShares();
+                const lastBalance = await rewardMaster.lastBalance();
+
+                const reward = unstakedAdvice.sharesToRedeem
+                    .mul(accumRewardPerShare)
+                    .div(accumRewardPerShareScale);
+
+                await expect(
+                    rewardMaster.connect(oracle).onAction(action, message),
+                )
+                    .to.emit(rewardMaster, 'RewardPaid')
+                    .withArgs(user_2.address, reward);
+
+                const {shares: updatedShares, offset: updatedOffset} =
+                    await rewardMaster.records(user_2.address);
+
+                const updatedLastBalance = await rewardMaster.lastBalance();
+
+                expect(await rewardMaster.totalShares()).to.be.eq(
+                    totalShares.sub(unstakedAdvice.sharesToRedeem),
+                );
+                expect(updatedLastBalance).to.be.eq(lastBalance.sub(reward));
+                expect(updatedShares).to.be.eq(
+                    shares.sub(unstakedAdvice.sharesToRedeem),
+                );
+                expect(updatedOffset).to.be.eq(0);
+
+                // nobody is sent tokens to contract directly
+                rewardToken.balanceOf.returns(updatedLastBalance);
             });
         });
+    });
 
-        describe('#entitled()', async () => {
-            before(async () => await deployRewardMaster());
+    describe('#entitled()', async () => {
+        const action = ethers.utils.id('STAKED_OR_UNSTAKED').slice(0, 10); // bytes4
+        const message = ethers.utils.id('AN_ENCODED_MESSAGE');
+        const releasableAmount = BigNumber.from(10).pow(24); // 1000000e18
+        const vestedRewards = releasableAmount;
+        const amountToStake = BigNumber.from(20).mul(
+            BigNumber.from(10).pow(18),
+        ); // 20e18: 20 ZKP tokens
+        const amountToShareScaledFactor = BigNumber.from(10);
+        const shareScale = BigNumber.from(1e3);
+        const accumRewardPerShareScale = BigNumber.from(1e9); // hardcoded in RewardMaster
 
-            it('should returns reward token amount entitled to the given user_1/account', async () => {
-                rewardPool.releasableAmount.returns(BigNumber.from(10).pow(18));
+        before(async () => {
+            await deployRewardMaster();
+            rewardPool.releasableAmount.returns(releasableAmount);
 
-                await rewardMaster.entitled(owner.address);
+            // add reward advisor
+            await rewardMaster
+                .connect(owner)
+                .addRewardAdviser(
+                    oracle.address,
+                    action,
+                    rewardAdviser.address,
+                );
+
+            const stakedAdvice = {
+                createSharesFor: 'replace_recipient_address_here',
+                sharesToCreate: amountToStake
+                    .mul(amountToShareScaledFactor)
+                    .div(shareScale),
+                redeemSharesFrom: ethers.constants.AddressZero,
+                sharesToRedeem: BigNumber.from(0),
+                sendRewardTo: ethers.constants.AddressZero,
+            };
+
+            // create shares for users
+            const users = [user_1, user_2, user_3, user_4];
+            for (let i = 0; i < users.length; i++) {
+                stakedAdvice.createSharesFor = users[i].address;
+                rewardAdviser.adviceReward.returns(stakedAdvice);
+
+                rewardToken.balanceOf.returns(vestedRewards.mul(i));
+
+                await rewardMaster.connect(oracle).onAction(action, message);
+            }
+
+            rewardPool.vestRewards.reset();
+        });
+
+        it('should returns reward token amount entitled to the user_1 when there is unreleased tokens', async () => {
+            const totalShares = await rewardMaster.totalShares();
+            const accumRewardPerShare =
+                await rewardMaster.accumRewardPerShare();
+
+            const newAccumRewardPerShare = accumRewardPerShare.add(
+                releasableAmount.mul(accumRewardPerShareScale).div(totalShares),
+            );
+
+            const {shares, offset} = await rewardMaster.records(user_1.address);
+
+            const reward = shares
+                .mul(newAccumRewardPerShare)
+                .div(accumRewardPerShareScale)
+                .sub(offset);
+
+            const entitled = await rewardMaster.entitled(user_1.address);
+
+            expect(entitled).to.be.eq(reward);
+        });
+
+        it('should returns reward token amount entitled to the user_1 when all vested tokens are released', async () => {
+            rewardPool.releasableAmount.returns(0);
+
+            const accumRewardPerShare =
+                await rewardMaster.accumRewardPerShare();
+
+            const {shares, offset} = await rewardMaster.records(user_1.address);
+
+            const reward = shares
+                .mul(accumRewardPerShare)
+                .div(accumRewardPerShareScale)
+                .sub(offset);
+
+            const entitled = await rewardMaster.entitled(user_1.address);
+
+            expect(entitled).to.be.eq(reward);
+        });
+
+        it('should returns 0 if user does not have shares', async () => {
+            const entitled = await rewardMaster.entitled(owner.address);
+
+            expect(entitled).to.be.eq(0);
+        });
+    });
+
+    describe('#claimErc20()', async () => {
+        const action = ethers.utils.id('STAKED_OR_UNSTAKED').slice(0, 10); // bytes4
+        const message = ethers.utils.id('AN_ENCODED_MESSAGE');
+
+        before(async () => await deployRewardMaster());
+
+        it('should not let admin withdraw reward tokens from contract when there are some shares', async () => {
+            // create shares for users
+            await rewardMaster
+                .connect(owner)
+                .addRewardAdviser(
+                    oracle.address,
+                    action,
+                    rewardAdviser.address,
+                );
+
+            // create shares for users
+            rewardAdviser.adviceReward.returns({
+                createSharesFor: user_1.address,
+                sharesToCreate: BigNumber.from(1000),
+                redeemSharesFrom: ethers.constants.AddressZero,
+                sharesToRedeem: BigNumber.from(0),
+                sendRewardTo: ethers.constants.AddressZero,
             });
+
+            await rewardMaster.connect(oracle).onAction(action, message);
+
+            await expect(
+                rewardMaster
+                    .connect(owner)
+                    .claimErc20(rewardToken.address, owner.address, 1),
+            ).to.be.revertedWith('RM: Failed to claim');
         });
     });
 });

7dc00d43c8bebb2cafa3b6fba1f4d961d256a087
nipun1999 <agarwalnipun12@gmail.com>
2022-01-23 18:13:55 +0530

    changed task to handle both increase and timestamp approach

--- contracts/tasks/time-increase.ts
+++ contracts/tasks/time-increase.ts
@@ -9,19 +9,26 @@ task(TASK_TIME_TRAVEL, 'time:increase on the blockchain')
         'Duration in seconds to increase the latest block timestamp',
     )
     .setAction(async (taskArgs, hre: HardhatRuntimeEnvironment) => {
-        const currentBlockNumber = await hre.ethers.provider.getBlockNumber();
-        const blockBefore = await hre.ethers.provider.getBlock(
-            currentBlockNumber,
-        );
-        const timestampBefore = blockBefore.timestamp;
-        console.log(`Current block timestamp ${timestampBefore}`);
-        await hre.ethers.provider.send('evm_mine', [
-            timestampBefore + +taskArgs.time,
-        ]);
-        const futureBlockNumber = await hre.ethers.provider.getBlockNumber();
-        const futureBlock = await hre.ethers.provider.getBlock(
-            futureBlockNumber,
-        );
-        const futureTimestamp = futureBlock.timestamp;
-        console.log(`Future block timestamp ${futureTimestamp}`);
+        const input = taskArgs.time;
+        if (input.includes('+')) {
+            const increaseTime = input.split('+')[1];
+            await hre.ethers.provider.send('evm_increaseTime', [+increaseTime]);
+            await hre.ethers.provider.send('evm_mine', []);
+            const futureBlockNumber =
+                await hre.ethers.provider.getBlockNumber();
+            const futureBlock = await hre.ethers.provider.getBlock(
+                futureBlockNumber,
+            );
+            const futureTimestamp = futureBlock.timestamp;
+            console.log(`Future block timestamp ${futureTimestamp}`);
+        } else {
+            await hre.ethers.provider.send('evm_mine', [+input]);
+            const futureBlockNumber =
+                await hre.ethers.provider.getBlockNumber();
+            const futureBlock = await hre.ethers.provider.getBlock(
+                futureBlockNumber,
+            );
+            const futureTimestamp = futureBlock.timestamp;
+            console.log(`Future block timestamp ${futureTimestamp}`);
+        }
     });

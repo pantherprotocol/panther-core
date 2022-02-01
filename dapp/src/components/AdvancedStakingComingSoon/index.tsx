4f7996036ccde21e33b71ba3fa94e7d181c43136
Adam Spiers <adam@pantherprotocol.io>
2022-02-01 21:34:10 +0000

    better text for Advanced staking box

--- dapp/src/components/AdvancedStakingComingSoon/index.tsx
+++ dapp/src/components/AdvancedStakingComingSoon/index.tsx
@@ -1,17 +1,28 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import Typography from '@mui/material/Typography';
+
+import {SafeLink} from '../../services/links';
+
 import './styles.scss';
 
 const AdvancedStakingComingSoon = () => {
     return (
         <Card className="advanced-staking-comming-soon-card" variant="outlined">
             <Typography className="card-title">
-                Advanced Staking Coming Soon
+                Advanced staking is on the way!
             </Typography>
             <Typography className="card-text">
-                Advanced incentivized ZKP private staking with a higher APY is
-                coming soon!
+                Now that the initial staking program is underway, the team is
+                working on advanced incentivized private staking with a higher
+                APY.
+            </Typography>
+            <Typography className="card-text">
+                This will showcase the first elements of the full protocol
+                involving zAssets and cross-chain interoperability.{' '}
+                <SafeLink href="https://blog.pantherprotocol.io/zkp-staking-will-be-available-this-wednesday-c73c7c98b5f2">
+                    Learn&nbsp;more
+                </SafeLink>
             </Typography>
         </Card>
     );

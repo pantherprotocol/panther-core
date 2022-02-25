73287ac5dbcbc7f086bede2e805d3902e840d16d
Adam Spiers <adam@pantherprotocol.io>
2022-02-25 14:31:38 +0000

    fix missing import of Redux store
    MR !108 introduced using of the Redux store without importing it.
    This broke the dapp build.  So add the missing import to fix the build.

--- dapp/src/index.tsx
+++ dapp/src/index.tsx
@@ -6,6 +6,7 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 
 import App from './App';
+import {store} from './redux/store';
 import reportWebVitals from './reportWebVitals';
 
 import './styles.scss';

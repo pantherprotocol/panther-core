08cc389307cada7c7be3c11f0d8c76620fa69021
Mayas Salman <mayassalman@gmail.com>
2022-03-06 12:01:08 +0200

    setting up redux toolkit skeleton in the project
    Without this patch, we won't be able to effectively handle data through the application.
    
    This is a problem as the app grows our data and logic behind it grows and we need a state management library to handle all of our logical layer.
    
    This patch solves the problem by adding redux stores with reducers and actions.
    
    Issue-URL: https://app.clickup.com/t/21pear6

---
+++ dapp/src/redux/hooks.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/named
+import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux';
+
+import {RootState, AppDispatch} from './store';
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;

pantherprotocol/panther-core

commit 6e9af880566c5086159390f7f0cc885e8692568f

dapp: staking method fixes - temp hiding staking method element
Without this patch, we will have staking method dropdown ui issue.

This is a problem because clicking on dropdown, it gets rendered wrongly with miss-placement and wrong styling.

This patch temporarily solves this issue by hiding this element.

dapp/src/components/StakeTab/index.tsx
@@ -131,7 +131,7 @@ export default function StakeTab(props: {
                 <CardContent>
                     <StakingInfoMSG />
                     {/* <Box display={'flex'} justifyContent={'center'}> */}
-                    <StakingMethod />
+                    {/* <StakingMethod /> */}
                     {/* </Box> */}
                 </CardContent>
             </Card>

commit 6f1509bbc2cd1f834ac72f430a1ee351f54167a9

dapp: fix eslint breakage from 6e9af880

dapp/src/components/StakeTab/index.tsx
@@ -7,8 +7,6 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Input from '@mui/material/Input';
 import InputAdornment from '@mui/material/InputAdornment';
-import MenuItem from '@mui/material/MenuItem';
-import Select from '@mui/material/Select';
 import Typography from '@mui/material/Typography';
 import {UnsupportedChainIdError, useWeb3React} from '@web3-react/core';
 import {NoEthereumProviderError} from '@web3-react/injected-connector';
@@ -360,33 +358,35 @@ const StakingInput = (props: {
     );
 };
 
-const StakingMethod = () => (
-    <Box className="staking-method-container">
-        <Box
-            display="flex"
-            justifyContent={'space-between'}
-            alignItems={'center'}
-        >
-            <Typography className="staking-method-title">
-                Staking Method:
-            </Typography>
-            <Select
-                labelId="staking-method-select-label"
-                id="staking-method-selectd"
-                variant="standard"
-                value={'Standard'}
-                className="staking-method-select"
-            >
-                <MenuItem selected value={'Standard'}>
-                    Standard
-                </MenuItem>
-                <MenuItem value={'Advanced'} disabled={true}>
-                    Advanced
-                </MenuItem>
-            </Select>
-        </Box>
-    </Box>
-);
+// Reintroduce when advanced staking is supported.
+//
+// const StakingMethod = () => (
+//     <Box className="staking-method-container">
+//         <Box
+//             display="flex"
+//             justifyContent={'space-between'}
+//             alignItems={'center'}
+//         >
+//             <Typography className="staking-method-title">
+//                 Staking Method:
+//             </Typography>
+//             <Select
+//                 labelId="staking-method-select-label"
+//                 id="staking-method-selectd"
+//                 variant="standard"
+//                 value={'Standard'}
+//                 className="staking-method-select"
+//             >
+//                 <MenuItem selected value={'Standard'}>
+//                     Standard
+//                 </MenuItem>
+//                 <MenuItem value={'Advanced'} disabled={true}>
+//                     Advanced
+//                 </MenuItem>
+//             </Select>
+//         </Box>
+//     </Box>
+// );
 
 const StakingInfoMSG = () => (
     <Box className="staking-info-container">
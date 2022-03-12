cc0991f52c7617122bf15ed3e18020f19d2646c7
Mayas Salman <mayassalman@gmail.com>
2022-03-12 21:07:31 +0200

    AlertDialog component for staking warning
    Without this patch, we can't warn stakers that if they stake now stake
    will not start accruing rewards until the switchover is complete.
    
    This is a problem because Staking is currently undergoing the
    switchover described in DAO proposal #4.
    
    This patch solves the problem by adding AlertDialog to warn stakers
    about this point before staking.
    
    Issue-URL: https://app.clickup.com/t/239y2qr
    
    Clickup: #239y2qr[in review]

---
+++ dapp/src/components/AlertDialog/index.tsx
@@ -0,0 +1,85 @@
+import * as React from 'react';
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import IconButton from '@mui/material/IconButton';
+import Stack from '@mui/material/Stack';
+import Tooltip from '@mui/material/Tooltip';
+import Typography from '@mui/material/Typography';
+
+import warningIcon from '../../images/warning-icon.svg';
+import xIcon from '../../images/x-icon.svg';
+import {SafeLink} from '../Common/links';
+
+import './styles.scss';
+
+export default function AlertDialog({handleClose, stake}) {
+    return (
+        <div>
+            <Dialog
+                open={true}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogContent className="alert-dialog-container">
+                    <Box className="x-icon">
+                        <Tooltip
+                            title={'Click to close Dialog box'}
+                            placement="top"
+                        >
+                            <IconButton onClick={handleClose}>
+                                <img src={xIcon} />
+                            </IconButton>
+                        </Tooltip>
+                    </Box>
+
+                    <Box className="warning-header">
+                        <IconButton onClick={handleClose}>
+                            <img src={warningIcon} />
+                        </IconButton>
+                        <Typography component="div" className="warning-text">
+                            Warning!
+                        </Typography>
+                    </Box>
+                    <Box>
+                        <Typography>
+                            Staking is currently undergoing the switchover
+                            described in{' '}
+                            <SafeLink href="https://docs.pantherprotocol.io/dao/governance/proposal-4-polygon-fix">
+                                DAO proposal #4.
+                            </SafeLink>
+                        </Typography>
+                        <Typography className=" message">
+                            If you stake now, your stake will not start accruing
+                            rewards until the switchover is complete.
+                        </Typography>
+                        <Typography>
+                            Are you sure you want to continue?
+                        </Typography>
+                    </Box>
+                    <Box className="buttons-holder">
+                        <Stack spacing={2} direction="row">
+                            <Button
+                                variant="outlined"
+                                className="button cancel-button"
+                                onClick={handleClose}
+                            >
+                                Cancel
+                            </Button>
+                            <Button
+                                variant="contained"
+                                className="button confirm-button"
+                                onClick={stake}
+                            >
+                                Stake anyway
+                            </Button>
+                        </Stack>
+                    </Box>
+                </DialogContent>
+            </Dialog>
+        </div>
+    );
+}

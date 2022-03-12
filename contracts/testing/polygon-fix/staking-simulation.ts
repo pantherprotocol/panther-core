6c63aee53b501d148b2f2b9237a5fd2a5806b734
pycckuu <is.markelov@gmail.com>
2022-03-12 22:02:29 +0100

    contracts: simulation improvements

--- contracts/testing/polygon-fix/staking-simulation.ts
+++ contracts/testing/polygon-fix/staking-simulation.ts
@@ -1,9 +1,21 @@
 import fs from 'fs';
 import stakedHistory from './data/staking_3.json';
 import {utils} from 'ethers';
+import {assert} from 'console';
 
 function getRandomInt(max: number) {
     return Math.floor(Math.random() * max);
+}
+
+function uuid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+        /[xy]/g,
+        function (c) {
+            var r = (Math.random() * 16) | 0,
+                v = c == 'x' ? r : (r & 0x3) | 0x8;
+            return v.toString(16);
+        },
+    );
 }
 
 function main() {
@@ -15,21 +27,75 @@ function main() {
 
         return {
             ...rec,
+            uuid: uuid(),
+            date: new Date(stakedAt * 1000),
             action: 'staking',
+            type: 'real',
             stakedAt,
             lockedTill: Number(rec.lockedTill),
             unstakedAt,
             amountDec: Number(utils.formatEther(rec.amount)),
             durationDays: (unstakedAt - stakedAt) / (24 * 3600),
-            stakedDate: rec.date,
+            stakedDate: new Date(stakedAt * 1000),
+            unstakedDate: new Date(unstakedAt * 1000),
             rewards: 0,
         };
     });
+
+    // Adding Simulated stakes:
+    const minStakeTimeForSimulatedStake = stakingRecords
+        .map(stakeRecord => stakeRecord.stakedAt)
+        .reduce((a, b) => Math.max(a, b), 0);
+
+    const maxUnstakingTimeForSimulatedStake = stakingRecords
+        .map(stakeRecord => stakeRecord.unstakedAt)
+        .reduce((a, b) => Math.max(a, b), 0);
+
+    const simulatedStakeNum = 15;
+    const lockedPeriod = 7 * 24 * 3600;
+    for (let i = 0; i < simulatedStakeNum; i++) {
+        const stakedAt =
+            minStakeTimeForSimulatedStake +
+            getRandomInt(
+                maxUnstakingTimeForSimulatedStake -
+                    minStakeTimeForSimulatedStake -
+                    lockedPeriod,
+            );
+
+        const lockedTill = stakedAt + lockedPeriod;
+        const unstakedAt =
+            lockedTill +
+            getRandomInt(maxUnstakingTimeForSimulatedStake - lockedTill);
+
+        assert(minStakeTimeForSimulatedStake < stakedAt);
+        assert(stakedAt < lockedTill);
+        assert(lockedTill < unstakedAt);
+        assert(unstakedAt < maxUnstakingTimeForSimulatedStake);
+
+        const stakedAmountDec = getRandomInt(10_000) + 100;
+        stakingRecords.push({
+            action: 'staking',
+            uuid: uuid(),
+            timestamp: stakedAt,
+            date: new Date(stakedAt * 1000),
+            type: 'simulated',
+            stakedAt,
+            lockedTill,
+            unstakedAt,
+            amountDec: stakedAmountDec,
+            amount: utils.parseUnits(stakedAmountDec.toString(), 18).toString(),
+            durationDays: (unstakedAt - stakedAt) / (24 * 3600),
+            stakedDate: new Date(stakedAt * 1000),
+            unstakedDate: new Date(unstakedAt * 1000),
+            rewards: 0,
+        });
+    }
 
     const unstakingRecords = stakingRecords.map((rec: any) => {
         return {
             ...rec,
             action: 'unstaking',
+            type: 'simulated',
             timestamp: Number(rec.unstakedAt),
         };
     });
@@ -39,12 +105,10 @@ function main() {
 
     const tokenPerSecond = 2000000 / 56 / 60 / 60 / 24;
     let currentlyStaked: any[] = [];
-    const startedTimestamp = 1646837699 - 100000; // Here we should probably have a timestamp of contract creation
-    let prevTimeStamp = startedTimestamp;
-
-    const rewards = actions.filter(
-        (action: any) => action.action === 'staking',
+    const startedTimestamp = Math.floor(
+        +new Date('Mon 7 Mar 23:59:59 UTC 2022') / 1000,
     );
+    let prevTimeStamp = startedTimestamp;
 
     actions.forEach((action: any) => {
         const totalAccumulatedRewards =
@@ -54,13 +118,6 @@ function main() {
             currentlyStaked.push(action);
         }
 
-        if (action.action === 'unstaking') {
-            currentlyStaked = currentlyStaked.filter(
-                (staked: any) =>
-                    staked.transactionHash !== action.transactionHash,
-            );
-        }
-
         const totalStaked = currentlyStaked
             .map((stake: any) => {
                 return stake.amountDec;
@@ -68,27 +125,32 @@ function main() {
             .reduce((a, b) => a + b, 0);
 
         currentlyStaked.forEach((staked: any) => {
-            const current = rewards.find((s: any) => {
-                return s.transactionHash === staked.transactionHash;
+            const current = actions.find((s: any) => {
+                return s.uuid === staked.uuid && s.action === 'unstaking';
             });
 
             current.rewards +=
                 (totalAccumulatedRewards * current.amountDec) / totalStaked;
         });
 
+        if (action.action === 'unstaking') {
+            currentlyStaked = currentlyStaked.filter(
+                (staked: any) => staked.uuid !== action.uuid,
+            );
+        }
+
         prevTimeStamp = action.timestamp;
     });
 
-    rewards.map((r: any) => {
+    actions.map((r: any) => {
         r.APY = (((r.rewards / r.amountDec) * 100) / r.durationDays) * 365;
     });
 
-    fs.writeFileSync('data/rewards.json', JSON.stringify(rewards, null, 2));
-    fs.writeFileSync('data/actions.json', JSON.stringify(actions, null, 2));
+    fs.writeFileSync('actions.json', JSON.stringify(actions, null, 2));
 
     console.log(
         'totalRewards',
-        rewards.map((s: any) => s.rewards).reduce((a, b) => a + b, 0),
+        actions.map((s: any) => s.rewards).reduce((a, b) => a + b, 0),
     );
 
     console.log(

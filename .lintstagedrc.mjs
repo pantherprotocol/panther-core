f306b818a1648d7d8f292a34d49eb099a984b5e8
Adam Spiers <adam@pantherprotocol.io>
2022-03-14 21:34:50 +0000

    lint-staged: work around eslintignore idiocy

---
+++ .lintstagedrc.mjs
@@ -0,0 +1,56 @@
+// import micromatch from 'micromatch';
+import {ESLint} from 'eslint';
+
+const removeIgnoredFiles = async (prefix, cfgFile, ignorePath, files) => {
+    const eslint = new ESLint({
+        overrideConfigFile: cfgFile,
+        ignorePath,
+    });
+    // console.log(eslint.calculateConfigForFile(files[0]));
+    const isIgnored = await Promise.all(
+        files.map(file => {
+            if (!file.startsWith(prefix)) {
+                console.error(
+                    `ERROR in .lintstagedrc.mjs: ${file} doesn't start with ${prefix}`,
+                );
+            }
+            const f = file.slice(prefix.length);
+            return eslint.isPathIgnored(f);
+        }),
+    );
+    const filteredFiles = files.filter((_, i) => !isIgnored[i]);
+    // const ignoredFiles = files.filter((_, i) => isIgnored[i]);
+    // console.log('Ignored files:', ignoredFiles);
+    return filteredFiles;
+};
+
+const run = (command, files) => {
+    if (files.length === 0) {
+        return `echo "Skipping; no files to run ${command}"`;
+    }
+    const cmd = `${command} ${files.join(' ')}`;
+    return cmd;
+};
+
+export default {
+    '*': files => {
+        return run('prettier --write', files);
+    },
+    'contracts/**/*.sol': files => {
+        return run('yarn workspace @zkp-staking/contracts lint:solhint', files);
+    },
+    'contracts/**/*.{js,ts}': async files => {
+        const prefix = process.cwd() + '/contracts/';
+        const toLint = await removeIgnoredFiles(
+            prefix,
+            'contracts/.eslintrc.json',
+            'contracts/.eslintignore',
+            files,
+        );
+        // const match = micromatch.not(files, '**/*.cli.js');
+        return run('yarn workspace @zkp-staking/contracts lint:eslint', toLint);
+    },
+    'dapp/**/*.{js,jsx,ts,tsx}': files => {
+        return run('yarn workspace @zkp-staking/dapp lint:eslint', files);
+    },
+};

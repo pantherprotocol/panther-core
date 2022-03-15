9d0cb15d0c51b2840e39b99b025fb06a56eb4f3b
Adam Spiers <adam@pantherprotocol.io>
2022-03-15 15:39:13 +0000

    contracts: add hardhat-deploy script for RewardTreasury

---
+++ contracts/deploy/06-RewardTreasury.ts
@@ -0,0 +1,21 @@
+import {DeployFunction} from 'hardhat-deploy/types';
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+    const {deployments, getNamedAccounts} = hre;
+    const {deploy} = deployments;
+    const {deployer} = await getNamedAccounts();
+
+    await deploy('RewardTreasury', {
+        from: deployer,
+        args: [
+            deployer, // owner
+            process.env.TOKEN_ADDRESS,
+        ],
+        log: true,
+        autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
+    });
+};
+export default func;
+
+func.tags = ['RewardTreasury'];

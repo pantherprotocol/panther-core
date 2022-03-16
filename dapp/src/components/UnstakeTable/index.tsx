cdf10e0cbc5d4030c352e4ca791b68f7b07a5043
Adam Spiers <adam@pantherprotocol.io>
2022-03-16 11:59:17 +0000

    dapp: add transition warnings to unstake tab

--- dapp/src/components/UnstakeTable/index.tsx
+++ dapp/src/components/UnstakeTable/index.tsx
@@ -1,7 +1,7 @@
 import {useCallback, useEffect, useState} from 'react';
 import * as React from 'react';
 
-import {Button, Tooltip} from '@mui/material';
+import {Tooltip, Button} from '@mui/material';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -9,12 +9,14 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
 import {useWeb3React} from '@web3-react/core';
 import {BigNumber} from 'ethers';
 
 import infoIcon from '../../images/info-icon.svg';
 import * as stakingService from '../../services/staking';
 import {formatTime, formatCurrency} from '../../utils/helpers';
+import {SafeLink} from '../Common/links';
 
 import './styles.scss';
 
@@ -38,12 +40,22 @@ const createStakedDataRow = (
     };
 };
 
+interface StakeRow {
+    id: number;
+    stakedAt: number;
+    amount: BigNumber;
+    calculatedReward: BigNumber;
+    lockedTill: number;
+    unstakable: boolean;
+    claimedAt: number;
+}
+
 function buildStakedDataRows(
     stakedData: any,
     rewardsBalance: BigNumber,
     totalStaked: BigNumber,
-) {
-    return stakedData.map(item => {
+): StakeRow[] {
+    return stakedData.map((item: StakeRow) => {
         const calculatedReward = formatCurrency(
             rewardsBalance.mul(item.amount).div(totalStaked),
             {decimals: 2},
@@ -93,7 +105,7 @@ export default function UnstakeTable(props: {fetchData: () => Promise<void>}) {
 
     const unstake = useCallback(
         async id => {
-            if (!library || !chainId || !account) {
+            if (!library || !chainId || !account || chainId === 137) {
                 return;
             }
 
@@ -120,6 +132,110 @@ export default function UnstakeTable(props: {fetchData: () => Promise<void>}) {
 
         setTotalStaked();
     }, [account, library, setTotalStaked]);
+
+    const transition = chainId === 137;
+    const transitionTooltipTitle = (
+        <>
+            <Typography paragraph={true}>
+                Due to the Polygon staking bug fix, there will be a transition
+                period after{' '}
+                <SafeLink href="https://docs.pantherprotocol.io/dao/governance/proposal-4-polygon-fix#proposed-actions">
+                    DAO proposal 4
+                </SafeLink>{' '}
+                voting closes during which the fix will be applied. During this
+                time unstaking will be temporarily unavailable.
+            </Typography>
+            <Typography paragraph={true}>
+                <strong>
+                    No rewards will be lost. Rewards will continue to accrue.
+                </strong>
+            </Typography>
+            <Typography paragraph={true}>
+                The transition period end is hard to predict due to the
+                decentralised nature of proposal execution, but it is{' '}
+                <em>likely</em> (but not guaranteed) to be less than 24 hours
+                from when the vote closes at Mar 16, 2022, 2:12 PM UTC.{' '}
+            </Typography>
+        </>
+    );
+
+    const TransitionTooltip = (props: any) => (
+        <Tooltip
+            title={transitionTooltipTitle}
+            data-html="true"
+            placement="top"
+            className="transitionTooltip"
+        >
+            {props.children}
+        </Tooltip>
+    );
+
+    const unstakeRow = (row: StakeRow) => {
+        const unstakeButton = (
+            <Button
+                className={`btn ${
+                    row.unstakable || transition ? 'disable' : ''
+                }`}
+                disabled={row.unstakable || transition}
+                onClick={() => {
+                    if (!transition) unstake(row.id);
+                }}
+                endIcon={transition && <img src={infoIcon} />}
+            >
+                Unstake
+            </Button>
+        );
+
+        return (
+            <React.Fragment key={row.stakedAt}>
+                {row.claimedAt == 0 && (
+                    <TableRow
+                        sx={{
+                            '&:last-child td, &:last-child th': {
+                                border: 0,
+                            },
+                        }}
+                    >
+                        <TableCell align="center">
+                            {formatTime(row.stakedAt)}
+                        </TableCell>
+                        <TableCell align="right">
+                            {formatCurrency(row.amount, {
+                                decimals: 2,
+                            })}{' '}
+                            ZKP
+                        </TableCell>
+                        {chainId !== 137 && (
+                            <TableCell align="right">
+                                {row.calculatedReward} ZKP
+                            </TableCell>
+                        )}
+                        <TableCell align="center" className="lockedTill">
+                            {formatTime(row.lockedTill)} <br />
+                            {transition && (
+                                <TransitionTooltip>
+                                    <span>
+                                        <SafeLink href="https://docs.pantherprotocol.io/dao/governance/proposal-4-polygon-fix#proposed-actions">
+                                            plus transition period
+                                        </SafeLink>
+                                    </span>
+                                </TransitionTooltip>
+                            )}
+                        </TableCell>
+                        <TableCell align="center" className="unstake">
+                            {transition ? (
+                                <TransitionTooltip>
+                                    <div>{unstakeButton}</div>
+                                </TransitionTooltip>
+                            ) : (
+                                unstakeButton
+                            )}
+                        </TableCell>
+                    </TableRow>
+                )}
+            </React.Fragment>
+        );
+    };
 
     return (
         <TableContainer component={Paper}>
@@ -151,52 +267,7 @@ export default function UnstakeTable(props: {fetchData: () => Promise<void>}) {
                         <TableCell align="center">Action</TableCell>
                     </TableRow>
                 </TableHead>
-                <TableBody>
-                    {stakedData.map(row => (
-                        <React.Fragment key={row.stakedAt}>
-                            {row.claimedAt == 0 && (
-                                <TableRow
-                                    sx={{
-                                        '&:last-child td, &:last-child th': {
-                                            border: 0,
-                                        },
-                                    }}
-                                >
-                                    <TableCell align="center">
-                                        {formatTime(row.stakedAt)}
-                                    </TableCell>
-                                    <TableCell align="right">
-                                        {formatCurrency(row.amount, {
-                                            decimals: 2,
-                                        })}{' '}
-                                        ZKP
-                                    </TableCell>
-                                    {chainId !== 137 && (
-                                        <TableCell align="right">
-                                            {row.calculatedReward} ZKP
-                                        </TableCell>
-                                    )}
-                                    <TableCell align="center">
-                                        {formatTime(row.lockedTill)}
-                                    </TableCell>
-                                    <TableCell align="center">
-                                        <Button
-                                            className={`btn ${
-                                                !row.unstakable ? '' : 'disable'
-                                            }`}
-                                            disabled={row.unstakable}
-                                            onClick={() => {
-                                                unstake(row.id);
-                                            }}
-                                        >
-                                            Unstake
-                                        </Button>
-                                    </TableCell>
-                                </TableRow>
-                            )}
-                        </React.Fragment>
-                    ))}
-                </TableBody>
+                <TableBody>{stakedData.map(unstakeRow)}</TableBody>
             </Table>
         </TableContainer>
     );

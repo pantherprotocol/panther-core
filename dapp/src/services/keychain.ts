4140cbce5ede0b0153cfe6375b7776446d4a921d
sulejman <107367+Sulejman@users.noreply.github.com>
2022-04-17 14:56:50 +0200

    dapp: adding keypair derivation method to services
    Without this patch, we cannot call keypair derivation from react layer in dapp.
    
    This is a problem because we need to generate root keypairs by clicking on advanced stake button in order to create staking input parameters.
    
    This patch solves the problem by implementing deriveKeypairFromSignature() in services layer and adding related tests to it.
    
    Issue-URL: https://app.clickup.com/t/2td9g2a

--- dapp/src/services/keychain.ts
+++ dapp/src/services/keychain.ts
@@ -1,4 +1,11 @@
-import {multiplyScalars, deriveKeypairFromSeed} from '../lib/keychain';
+import {poseidon} from 'circomlibjs';
+import {Signer} from 'ethers';
+
+import {
+    deriveKeypairFromSignature,
+    multiplyScalars,
+    deriveKeypairFromSeed,
+} from '../lib/keychain';
 import {IKeypair, PrivateKey} from '../lib/types';
 
 // generateSpendingChildKeypair generates child spending keypair (s', S')
@@ -9,4 +16,15 @@ export function generateSpendingChildKeypair(
 ): IKeypair {
     const spendingChildPrivKey = multiplyScalars(rootSpendingPrivKey, r);
     return deriveKeypairFromSeed(spendingChildPrivKey);
+}
+
+//TODO: Derivation message content to be defined in https://app.clickup.com/t/26gtpr4
+export async function deriveRootKeypairs(signer: Signer): Promise<IKeypair[]> {
+    const derivationMessage = `I'm creating a spending and reading keypairs for ${await signer.getAddress()}`;
+    const signature = await signer.signMessage(derivationMessage);
+    const hashedSignature = poseidon([signature]);
+    return [
+        deriveKeypairFromSignature(signature),
+        deriveKeypairFromSeed(hashedSignature),
+    ];
 }

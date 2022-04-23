50f3276249e78186b371065a82881b3d56b4aff3
Vadim Konstantinov <vadim.konstantinov@gmail.com>
2022-04-23 11:12:59 +0300

    contracts: placeholder-libs in Poseidon.sol revert if used
    to avoid accentual use of the placeholder-libs instead of the implementation from the ciromlibjs

--- panther-protocol-contracts-ws/contracts/Poseidon.sol
+++ panther-protocol-contracts-ws/contracts/Poseidon.sol
@@ -2,21 +2,20 @@
 pragma solidity ^0.8.4;
 
 // This is a stub to keep solc happy; the actual code is generated
-// using poseidon_gencontract.js from circomlib.
+// using poseidon_gencontract.js from circomlibjs.
 
-// solhint-disable no-empty-blocks
 library PoseidonT3 {
-    function poseidon(bytes32[2] memory input)
-        external
-        pure
-        returns (bytes32)
-    {}
+    // solhint-disable-next-line no-unused-vars
+    function poseidon(bytes32[2] memory input) external pure returns (bytes32) {
+        revert("FAKE");
+        return 0;
+    }
 }
 
 library PoseidonT4 {
-    function poseidon(bytes32[3] memory input)
-        external
-        pure
-        returns (bytes32)
-    {}
+    // solhint-disable-next-line no-unused-vars
+    function poseidon(bytes32[3] memory input) external pure returns (bytes32) {
+        revert("FAKE");
+        return 0;
+    }
 }

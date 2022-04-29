b64c0b6cf5a731c2658043ce183b9e2c463b66d3
Vadim Konstantinov <vadim.konstantinov@gmail.com>
2022-04-29 15:53:16 +0300

    contracts: Constant.sol and ErrorMsgs.sol cleaned up

--- panther-protocol-contracts-ws/contracts/common/Constants.sol
+++ panther-protocol-contracts-ws/contracts/common/Constants.sol
@@ -9,18 +9,10 @@ uint256 constant IN_UTXOs = 2 + IN_PRP_UTXOs;
 uint256 constant OUT_PRP_UTXOs = 1;
 uint256 constant OUT_UTXOs = 2 + OUT_PRP_UTXOs;
 
-uint256 constant UTXO_SECRETS = 6;
+uint256 constant UTXO_SECRETS_T0 = 4;
+uint256 constant UTXO_SECRETS_T1 = 3;
 
 // For overflow protection and circuits optimization
-uint256 constant MAX_EXT_AMOUNT = 2**120;
-/*
-TODO: decide if `MAX_EXT_AMOUNT` may/shall be weaken/tightened
-// Addition of up to 32 numbers in circuits must not overflow
-// i.e. MAX_EXT_AMOUNT*32 < snark_field_prime
-uint256 public constant MAX_EXT_AMOUNT = 2**248;
-//
-// It works with "scalingFactor" (see EIP-1724 to get an idea)
-uint256 public constant MAX_EXT_AMOUNT = 2**32;
-*/
+uint256 constant MAX_EXT_AMOUNT = 2**96;
 
 uint256 constant MAX_TIMESTAMP = 2**32;

3e37bda27affdbf8d1b557b459948c2c66981146
Roman M <roman@pantherprotocol.io>
2022-05-09 18:21:43 +0300

    fix: libs stuff

---
+++ panther-protocol-contracts-ws/lib/keychain.ts
@@ -0,0 +1,135 @@
+/*
+Some of the functions are modified version or inspired by this code:
+https://github.com/appliedzkp/maci/blob/master/crypto/ts/index.ts
+*/
+
+import assert from 'assert';
+import crypto from 'crypto';
+
+import createBlakeHash from 'blake-hash';
+import {babyjub, eddsa, poseidon} from 'circomlibjs';
+import * as ff from 'ffjavascript';
+
+export interface IKeypair {
+    publicKey: bigint[];
+    privateKey: bigint;
+}
+export type PrivateKey = bigint;
+export type PublicKey = bigint[];
+export type EcdhSharedKey = bigint;
+//import {IKeypair, PrivateKey, PublicKey} from './types';
+
+export const SNARK_FIELD_SIZE = BigInt(
+    '21888242871839275222246405745257275088548364400416034343698204186575808495617',
+);
+
+export const deriveKeypairFromSeed = (
+    seed = generateRandomBabyJubValue(),
+): IKeypair => {
+    const privateKey = truncateToSnarkField(seed); //
+    const publicKey = generatePublicKey(privateKey);
+    return {
+        privateKey: privateKey,
+        publicKey: publicKey,
+    };
+};
+
+export const multiplyScalars = (a: bigint, b: bigint): bigint => {
+    return (a * b) % babyjub.subOrder;
+};
+
+export const deriveKeypairFromSignature = (signature: string): IKeypair => {
+    const pKey = derivePrivateKeyFromSignature(signature);
+    return deriveKeypairFromSeed(pKey);
+};
+
+export const truncateToSnarkField = (v: bigint): bigint => {
+    return v % SNARK_FIELD_SIZE;
+};
+
+export const generatePublicKey = (privateKey: PrivateKey): PublicKey => {
+    assert(privateKey < SNARK_FIELD_SIZE);
+    return babyjub.mulPointEscalar(
+        babyjub.Base8,
+        formatPrivateKeyForBabyJub(privateKey),
+    );
+};
+
+export const formatPrivateKeyForBabyJub = (privateKey: PrivateKey) => {
+    const sBuff = eddsa.pruneBuffer(
+        createBlakeHash('blake512')
+            .update(bigIntToBuffer(privateKey))
+            .digest()
+            .slice(0, 32),
+    );
+    const s = ff.utils.leBuff2int(sBuff);
+    return ff.Scalar.shr(s, 3);
+};
+
+const generateRandomness = (): bigint => {
+    const min = BigInt(
+        '6350874878119819312338956282401532410528162663560392320966563075034087161851',
+    );
+    let randomness;
+    while (true) {
+        randomness = BigInt('0x' + crypto.randomBytes(32).toString('hex'));
+        if (randomness >= min) {
+            break;
+        }
+    }
+    return randomness;
+};
+
+const bigIntToBuffer = (i: BigInt): Buffer => {
+    let hexStr = i.toString(16);
+    while (hexStr.length < 64) {
+        hexStr = '0' + hexStr;
+    }
+    return Buffer.from(hexStr, 'hex');
+};
+
+export const generateRandomBabyJubValue = (): bigint => {
+    const random = generateRandomness();
+    const privateKey: PrivateKey = random % SNARK_FIELD_SIZE;
+    assert(privateKey < SNARK_FIELD_SIZE);
+    return privateKey;
+};
+
+export const extractSecretsPair = (
+    signature: string,
+): [r: bigint, s: bigint] => {
+    if (!signature) {
+        throwKeychainError('Signature must be provided');
+    }
+    if (signature.length !== 132) {
+        throwKeychainError(
+            `Tried to create keypair from signature of length '${signature.length}'`,
+        );
+    }
+    if (signature.slice(0, 2) !== '0x') {
+        throwKeychainError(
+            `Tried to create keypair from signature without 0x prefix`,
+        );
+    }
+    // We will never verify this signature; we're only using it as a
+    // deterministic source of entropy which can be used in a ZK proof.
+    // So we can discard the LSB v which has the least entropy.
+    const r = signature.slice(2, 66);
+    const s = signature.slice(66, 130);
+    return [
+        BigInt('0x' + r) % SNARK_FIELD_SIZE,
+        BigInt('0x' + s) % SNARK_FIELD_SIZE,
+    ];
+};
+
+export const derivePrivateKeyFromSignature = (signature: string): bigint => {
+    const pair = extractSecretsPair(signature);
+    if (!pair) {
+        throwKeychainError('Failed to extract secrets pair from signature');
+    }
+    return poseidon(pair);
+};
+
+const throwKeychainError = (errMsg: string) => {
+    throw new Error(`Keychain error: ${errMsg}`);
+};
---
+++ panther-protocol-contracts-ws/lib/message-encryption.ts
@@ -0,0 +1,54 @@
+import crypto from 'crypto';
+
+import {babyjub} from 'circomlibjs';
+import {utils} from 'ethers';
+
+import {bigintToBytes32} from './conversions';
+import {formatPrivateKeyForBabyJub} from './keychain';
+import {ICiphertext, PrivateKey, PublicKey, EcdhSharedKey} from './types';
+
+export const generateEcdhSharedKey = (
+    privateKey: PrivateKey,
+    publicKey: PublicKey,
+): EcdhSharedKey => {
+    return babyjub.mulPointEscalar(
+        publicKey,
+        formatPrivateKeyForBabyJub(privateKey),
+    )[0];
+};
+
+export function encryptMessage(
+    plaintext: string,
+    sharedKey: EcdhSharedKey,
+): ICiphertext {
+    const iv = crypto.randomBytes(16);
+
+    try {
+        const cipher = crypto.createCipheriv(
+            'aes-256-cbc',
+            utils.arrayify(bigintToBytes32(sharedKey)),
+            iv,
+        );
+        return {
+            iv: iv.toString('hex'),
+            data: cipher.update(plaintext, 'utf8', 'hex') + cipher.final('hex'),
+        };
+    } catch (error) {
+        throw Error(`Failed to encrypt message: ${error}`);
+    }
+}
+
+export function decryptMessage(
+    ciphertext: ICiphertext,
+    sharedKey: EcdhSharedKey,
+): string {
+    const decipher = crypto.createDecipheriv(
+        'aes-256-cbc',
+        utils.arrayify(bigintToBytes32(sharedKey)),
+        Buffer.from(ciphertext.iv, 'hex'),
+    );
+
+    return (
+        decipher.update(ciphertext.data, 'hex', 'utf8') + decipher.final('utf8')
+    );
+}

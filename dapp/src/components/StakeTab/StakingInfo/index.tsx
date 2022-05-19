bd150eeef924ad2aa335fc0122b4faccba0582cc
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2022-05-19 23:13:02 +0200

    dapp: fixing stake/unstake info messages
    https://app.clickup.com/t/2bff49a

--- dapp/src/components/StakeTab/StakingInfo/index.tsx
+++ dapp/src/components/StakeTab/StakingInfo/index.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {ReactElement, useCallback} from 'react';
 
-import {Box, Typography} from '@mui/material';
+import {Typography, Card, CardContent} from '@mui/material';
 import {useWeb3React} from '@web3-react/core';
 
 import {useAppSelector} from '../../../redux/hooks';
-import {isStakingOpenSelector} from '../../../redux/slices/stakeTerms';
+import {
+    isStakingOpenSelector,
+    termsSelector,
+} from '../../../redux/slices/stakeTerms';
+import {chainHasAdvancedStaking} from '../../../services/contracts';
 import {StakeType} from '../../../types/staking';
+import {formatTime} from '../../../utils/helpers';
 import {SafeMuiLink} from '../../Common/links';
 
 import './styles.scss';
@@ -13,75 +18,184 @@ import './styles.scss';
 export default function StakingInfo() {
     const context = useWeb3React();
     const {chainId} = context;
-    const isStakingOpen = useAppSelector(
+
+    const isAdvancedStakingOpen = useAppSelector(
         isStakingOpenSelector(chainId!, StakeType.Advanced),
     );
 
-    const subtitle = isStakingOpen ? (
-        <>Staking will lock your tokens for a minimum of 7 days </>
-    ) : (
-        <>Classic staking is closed for new stakes</>
+    const stakeType = chainHasAdvancedStaking(chainId)
+        ? StakeType.Advanced
+        : StakeType.Classic;
+
+    const allowedTill = useAppSelector(
+        termsSelector(chainId!, stakeType, 'allowedTill'),
     );
 
-    const text = isStakingOpen ? (
-        <>
-            You will need to unstake to collect your rewards. Rewards are not
-            automatically staked. Unstaking is available after 7 days.
-        </>
-    ) : (
-        <>
-            <Typography>
-                The classic staking rewards program ended on May 4th, so new
-                stakes were automatically disabled by the smart contracts on
-                April 27th as{' '}
-                <SafeMuiLink
-                    href="https://docs.pantherprotocol.io/launchdao/voting-proposals/3-launch/staking"
-                    underline="always"
-                    color="inherit"
-                >
-                    previously
-                </SafeMuiLink>{' '}
-                <SafeMuiLink
-                    href="https://docs.pantherprotocol.io/dao/governance/proposal-3-polygon-extension/staking"
-                    underline="always"
-                    color="inherit"
-                >
-                    scheduled
-                </SafeMuiLink>
-                .
-            </Typography>
-            <p>
-                You can still unstake{' '}
-                <SafeMuiLink
-                    href="https://docs.pantherprotocol.io/dao/support/faq/staking#when-unstake"
-                    underline="always"
-                    color="inherit"
-                >
-                    at any time
-                </SafeMuiLink>
-                , and there is no deadline for claiming rewards. However active
-                stakes have ceased to earn further rewards.
-            </p>
-            <p>
-                Also, advanced staking is now close!{' '}
-                <SafeMuiLink
-                    href="https://blog.pantherprotocol.io/advanced-staking-is-on-its-way-heres-how-to-prepare-for-it-b14cd01e4cc4"
-                    underline="always"
-                    color="inherit"
-                >
-                    Read more
-                </SafeMuiLink>
-                .
-            </p>
-        </>
+    const lockedTill = useAppSelector(
+        termsSelector(chainId!, stakeType, 'lockedTill'),
     );
+
+    const minLockPeriod = useAppSelector(
+        termsSelector(chainId!, stakeType, 'minLockPeriod'),
+    );
+
+    const getAdvancedStakingOpenText = useCallback((): {
+        subtitle: string;
+        body: ReactElement;
+    } => {
+        let subtitle = 'Advanced staking is open';
+        if (allowedTill) {
+            const allowedTillDate = formatTime(Number(allowedTill) * 1000);
+            subtitle += ` till ${allowedTillDate}`;
+        }
+        const body = (
+            <Typography>
+                Advanced Staking will lock your tokens{' '}
+                {lockedTill &&
+                    `until ${formatTime(Number(lockedTill) * 1000)} `}
+                and create zZKP as rewards in the Multi-Asset Shielded Pool
+                (MASP). By staking your ZKP, you become one of the first people
+                to create zAssets and contribute to bootstrapping and testing of
+                the MASP.
+            </Typography>
+        );
+
+        return {subtitle, body};
+    }, [allowedTill, lockedTill]);
+
+    const getAdvancedStakingClosedText = useCallback((): {
+        subtitle: string;
+        body: ReactElement;
+    } => {
+        return {
+            subtitle: 'Advanced staking is closed for new stakes',
+            body: (
+                <>
+                    <Typography>
+                        The advanced staking rewards program ended
+                        {allowedTill
+                            ? ' on ' + formatTime(Number(allowedTill) * 1000)
+                            : ''}
+                        , so new stakes were automatically disabled by the smart
+                        contracts.
+                    </Typography>
+                    <p>
+                        You can still unstake{' '}
+                        <SafeMuiLink
+                            href="https://docs.pantherprotocol.io/dao/support/faq/staking#when-unstake"
+                            underline="always"
+                            color="inherit"
+                        >
+                            at any time
+                        </SafeMuiLink>
+                        , and there is no deadline for claiming rewards. However
+                        active stakes have ceased to earn further rewards.
+                    </p>
+                </>
+            ),
+        };
+    }, [allowedTill]);
+
+    const getClassicStakingClosedText = useCallback((): {
+        subtitle: string;
+        body: ReactElement;
+    } => {
+        const link = (() => {
+            switch (chainId) {
+                case 1:
+                case 4:
+                    return 'https://docs.pantherprotocol.io/launchdao/voting-proposals/3-launch/staking';
+                case 137:
+                case 80001:
+                    return 'https://docs.pantherprotocol.io/dao/governance/proposal-3-polygon-extension/staking';
+                default:
+                    return '';
+            }
+        })();
+
+        console.log('allowedTill: ', allowedTill);
+        console.log('minLockPeriod: ', minLockPeriod);
+        const rewardsEnd = Number(allowedTill) + Number(minLockPeriod);
+
+        return {
+            subtitle: 'Classic staking is closed for new stakes',
+            body: (
+                <>
+                    <Typography>
+                        The classic staking rewards program ended on{' '}
+                        {formatTime(rewardsEnd * 1000)}, so new stakes were
+                        automatically disabled by the smart contracts on{' '}
+                        {formatTime(Number(allowedTill) * 1000)}
+                        {link && (
+                            <>
+                                {' '}
+                                as{' '}
+                                <SafeMuiLink
+                                    href={link}
+                                    underline="always"
+                                    color="inherit"
+                                >
+                                    previously scheduled
+                                </SafeMuiLink>
+                            </>
+                        )}
+                        .
+                    </Typography>
+                    <p>
+                        You can still unstake{' '}
+                        <SafeMuiLink
+                            href="https://docs.pantherprotocol.io/dao/support/faq/staking#when-unstake"
+                            underline="always"
+                            color="inherit"
+                        >
+                            at any time
+                        </SafeMuiLink>
+                        , and there is no deadline for claiming rewards. However
+                        active stakes have ceased to earn further rewards.
+                    </p>
+                    <p>
+                        Also, advanced staking is now in public testing!{' '}
+                        <SafeMuiLink
+                            href="https://blog.pantherprotocol.io/incentivized-testing-for-zkp-advanced-staking-is-now-live-19b51bc6b42b"
+                            underline="always"
+                            color="inherit"
+                        >
+                            Read more
+                        </SafeMuiLink>
+                        .
+                    </p>
+                </>
+            ),
+        };
+    }, [chainId, allowedTill, minLockPeriod]);
+
+    const getAdvancedStakingText = useCallback((): {
+        subtitle: string;
+        body: ReactElement;
+    } => {
+        if (isAdvancedStakingOpen) {
+            return getAdvancedStakingOpenText();
+        }
+
+        return getAdvancedStakingClosedText();
+    }, [
+        isAdvancedStakingOpen,
+        getAdvancedStakingOpenText,
+        getAdvancedStakingClosedText,
+    ]);
+
+    const {subtitle, body} = chainHasAdvancedStaking(chainId)
+        ? getAdvancedStakingText()
+        : getClassicStakingClosedText();
 
     return (
-        <Box className="staking-info-container">
-            <Typography variant="subtitle2" className="staking-info-title">
-                {subtitle}
-            </Typography>
-            <Box className="staking-info-text">{text}</Box>
-        </Box>
+        <Card variant="outlined" className="staking-info-container">
+            <CardContent className="staking-info-card-content">
+                <Typography variant="subtitle2" className="staking-info-title">
+                    {subtitle}
+                </Typography>
+                <Typography className="staking-info-text">{body}</Typography>
+            </CardContent>
+        </Card>
     );
 }

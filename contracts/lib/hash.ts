cf25a16e09a53476893933b6d14d251ef6e759c8
bitnician <whoisbehzad@gmail.com>
2022-06-14 16:52:26 +0430

    Add a missing `advancedActionHash()` to `lib/hash.ts`
    Without this patch, there is no `advancedActionHash()` in `lib/hash.ts`.
    
    This is a problem because it has been imported into another module and without it, running contracts locally throws an error.
    
    This patch solves the problem by adding the missing function to `hash.ts`.
    The `combinedHash(action: string, type: string)` is also added as a helper function to calculate the hash of `action` and `type` together and then returns the first 10 bytes of the hash (`0x` + 8 bytes). The `advancedActionHash()` and `classicActionHash()` functions calls this helper function to get the hash either for `ADVANCED` type or `CLASSIC` type.
    
    Issue-URL: https://app.clickup.com/t/2dzeddq

--- contracts/lib/hash.ts
+++ contracts/lib/hash.ts
@@ -5,13 +5,20 @@ export const hash4bytesArray = (s: string) =>
     utils.arrayify(utils.id(s)).slice(0, 4);
 
 export const CLASSIC = 'classic';
+export const ADVANCED = 'advanced';
 export const STAKE = 'stake';
 export const UNSTAKE = 'unstake';
 
-export const classicActionHash = (action: string) => {
+const combinedHash = (action: string, type: string) => {
     const combined = utils.concat([
         hash4bytesArray(action),
-        hash4bytesArray(CLASSIC),
+        hash4bytesArray(type),
     ]);
     return utils.keccak256(combined).slice(0, 10);
 };
+
+export const classicActionHash = (action: string) =>
+    combinedHash(action, CLASSIC);
+
+export const advancedActionHash = (action: string) =>
+    combinedHash(action, ADVANCED);

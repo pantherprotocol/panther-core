a1b33070a7a40d4393b4d5a5e80cb9c38ca4feca
Sajjad Heydari <sajjadheydari1401@gmail.com>
2022-06-16 15:45:58 +0430

    Listing contracts in /contracts page to show addresses per chain
    Without this patch, we lack a handy component for debugging our contract related issues.
    
    This is a problem because we must know exactly how many contracts we have deployed per chain in order to have the big picture of the whole process.
    
    This patch solves the problem by using special functions & hooks like getContractAddress from services/contracts.ts file
    
    Issue-URL: https://app.clickup.com/t/2345xjb

---
+++ dapp/src/pages/Contracts/index.tsx
@@ -0,0 +1,135 @@
+import React, {useCallback, useEffect, useState} from 'react';
+
+import {
+    Box,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Typography,
+} from '@mui/material';
+import {useWeb3React} from '@web3-react/core';
+
+import {addressLink, SafeLink} from '../../components/Common/links';
+import {MainPageWrapper} from '../../components/MainPageWrapper';
+import background from '../../images/background.png';
+import {supportedNetworks} from '../../services/connectors';
+import {
+    ContractName,
+    getContractAddress,
+    hasContract,
+} from '../../services/contracts';
+
+import './styles.scss';
+
+const ContractsPage = () => {
+    const context = useWeb3React();
+    const {chainId} = context;
+
+    const [contracts, setContracts] = useState<object[]>([]);
+
+    const loadContracts = useCallback(() => {
+        const contractsArr: object[] = [];
+
+        const values = Object.values(ContractName);
+        const contractNames: any = values.filter(
+            contractName => typeof contractName === 'string',
+        );
+        contractNames.map((cName: any) => {
+            const hasContractAddress = hasContract(
+                ContractName[cName] as unknown as ContractName,
+                chainId!,
+            );
+            if (hasContractAddress) {
+                const address = getContractAddress(
+                    ContractName[cName] as unknown as ContractName,
+                    chainId!,
+                );
+                const url = addressLink(chainId!, address);
+
+                const contractsObject = {
+                    name: cName,
+                    address: address,
+                    url: url,
+                };
+                contractsArr.push(contractsObject);
+            }
+        });
+
+        setContracts(contractsArr);
+    }, [chainId]);
+
+    useEffect(() => {
+        loadContracts();
+    }, [loadContracts]);
+
+    return (
+        <Box className="contracts-page-container">
+            <MainPageWrapper background={background}>
+                <Typography className="table-title">
+                    {chainId
+                        ? `Contracts on ${supportedNetworks[chainId!]?.name}`
+                        : 'Please Connect Your Wallet!'}
+                </Typography>
+                <TableContainer
+                    component={Paper}
+                    className="contracts-table-container"
+                >
+                    <Table
+                        sx={{minWidth: 650}}
+                        aria-label="simple table"
+                        className="contracts-table"
+                    >
+                        <TableHead>
+                            {chainId && (
+                                <TableRow>
+                                    <TableCell>
+                                        <Typography className="table-head">
+                                            Contract Name
+                                        </Typography>
+                                    </TableCell>
+                                    <TableCell align="right">
+                                        <Typography className="table-head">
+                                            Contract Address
+                                        </Typography>
+                                    </TableCell>
+                                </TableRow>
+                            )}
+                        </TableHead>
+                        <TableBody>
+                            {contracts &&
+                                contracts.map((row: any) => (
+                                    <TableRow
+                                        key={row.name}
+                                        sx={{
+                                            '&:last-child td, &:last-child th':
+                                                {
+                                                    border: 0,
+                                                },
+                                        }}
+                                    >
+                                        <TableCell component="th" scope="row">
+                                            {row.name}
+                                        </TableCell>
+                                        <TableCell
+                                            className="contract-address"
+                                            align="right"
+                                        >
+                                            <SafeLink href={row.url}>
+                                                {row.address}
+                                            </SafeLink>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </MainPageWrapper>
+        </Box>
+    );
+};
+
+export default ContractsPage;

def23424db500818fd5de41530cd5cd4bd199749
bitnician <whoisbehzad@gmail.com>
2022-06-28 14:56:56 +0430

    Modify `tems:add` task to support both advance and classic terms.
    Without this patch, staking terms can only be added based on  the terms which have been hardcoded in the `terms-add.ts` task. These hardcoded terms only contains classic params and advanced params have been missed. Furthermore, there is no possibility to pass terms as json to overwrite the default terms which have been hardcoded in the `terms-add.ts`.
    
    This patch solves the problem by refactoring the task to read the terms from the JSON  which contains at least one kind of staking terms: `classic` and `advanced`. Default terms are used if no JSON is provided.
    
    An example of executing task using json terms:
    ```
    echo '{"allowedSince":"24 Jun 2022 12:00:00 GMT","allowedTill":"25 Jun 2022 12:00:00 GMT","lockedTill":"26 Jun 2022 12:00:00 GMT"}' | hh terms:add --json true --network <netwrok>
    ```
    
    Issue-URL: https://app.clickup.com/t/2cx5nbd

--- contracts/tasks/terms-add.ts
+++ contracts/tasks/terms-add.ts
@@ -1,32 +1,98 @@
+import fs from 'fs';
+import {Staking, IStakingTypes} from './../types/contracts/Staking';
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {task, types} from 'hardhat/config';
-import {utils} from 'ethers';
+import {parseDate} from '../lib/units-shortcuts';
+import {addTerms, StakeType} from '../lib/staking';
 
 const TASK_TERMS_ADD = 'terms:add';
 
-task(TASK_TERMS_ADD, 'Adds terms to staking contract for classic staking')
-    .addOptionalParam(
-        'rewarded',
-        'Whether reward is enabled or not',
-        true,
+function normalizeTermParams(_params: any) {
+    const params = {..._params};
+
+    for (const param of ['allowedSince', 'allowedTill', 'lockedTill']) {
+        if (params[param] && typeof params[param] === 'string') {
+            params[param] = parseDate(params[param]);
+        }
+    }
+
+    return params;
+}
+
+function getDefaultTermParams(): IStakingTypes.TermsStruct {
+    return {
+        isEnabled: true,
+        isRewarded: true,
+        minAmountScaled: 0,
+        maxAmountScaled: 0,
+        allowedSince: 0,
+        allowedTill: 0,
+        lockedTill: 0,
+        exactLockPeriod: 0,
+        minLockPeriod: 3600,
+    };
+}
+
+function getDefaultClassicParams() {
+    return {...getDefaultTermParams(), minLockPeriod: 86400};
+}
+
+function getDefaultAdvancedParams() {
+    const now = Math.floor(new Date().getTime() / 1000);
+    const day = 86400;
+    const hour = 3600;
+
+    const allowedTill = now + day * 5;
+    const lockedTill = allowedTill + hour * 12; // 12 hours after allowedTill
+
+    return {
+        ...getDefaultTermParams(),
+        minAmountScaled: 100,
+        allowedSince: now,
+        allowedTill,
+        lockedTill,
+    };
+}
+
+function getTerms(
+    stakeType: StakeType,
+    useJSON: boolean,
+): IStakingTypes.TermsStruct {
+    const overrides = useJSON ? JSON.parse(fs.readFileSync(0).toString()) : {};
+    let defaults: IStakingTypes.TermsStruct;
+
+    if (stakeType == 'advanced') {
+        defaults = getDefaultAdvancedParams();
+    } else if (stakeType == 'classic') {
+        defaults = getDefaultClassicParams();
+    } else {
+        throw new Error(`Unknown type: ${stakeType}`);
+    }
+
+    return {
+        ...normalizeTermParams(defaults),
+        ...normalizeTermParams(overrides),
+    };
+}
+
+task(TASK_TERMS_ADD, 'Adds terms to staking contract')
+    .addParam(
+        'type',
+        'Defines the staking type that terms will be added for. it can be classic or advanced.',
+        undefined,
+        types.string,
+    )
+    .addParam(
+        'json',
+        'Accept pool parameters from STDIN in JSON format',
+        false,
         types.boolean,
     )
-    .setAction(async (taskArgs, hre: HardhatRuntimeEnvironment) => {
-        const staking = await hre.ethers.getContract('Staking');
-        const terms = {
-            isEnabled: true,
-            isRewarded: taskArgs.rewarded,
-            minAmountScaled: utils.hexZeroPad('0x00', 32),
-            maxAmountScaled: utils.hexZeroPad('0x00', 32),
-            allowedSince: utils.hexZeroPad('0x00', 32),
-            allowedTill: utils.hexZeroPad('0x00', 32),
-            lockedTill: utils.hexZeroPad('0x00', 32),
-            exactLockPeriod: utils.hexZeroPad('0x00', 32),
-            minLockPeriod: utils.hexZeroPad('0x1E', 32),
-        };
-        const stakeType = '0x4ab0941a'; // bytes4(keccak('classic'))
 
-        const tx = await staking.addTerms(stakeType, terms);
-        const receipt = await tx.wait();
-        console.log('addTerms transaction receipt:', receipt);
+    .setAction(async (taskArgs, hre: HardhatRuntimeEnvironment) => {
+        const terms = getTerms(taskArgs.type, taskArgs.json);
+
+        const staking = (await hre.ethers.getContract('Staking')) as Staking;
+
+        await addTerms(staking, terms, taskArgs.type);
     });

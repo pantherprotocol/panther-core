9568228bf3e55e10f88bba1a4c104a8c7d340d2c
bitnician <whoisbehzad@gmail.com>
2022-06-28 14:16:01 +0430

    Add a `TestnetStaking` contract Without this patch, a new staking contract needs to be deployed for each test period.
    This is a problem because there is no function to update the staking terms. so if the staking period is finished, the new contract needs to be deployed.
    
    This patch solves the problem by adding a `TestnetStaking` contract that inherits `Staking` and includes the `updateTerms()`
    
    To update the contract using block explorer, these values as template:
    ```
    _stakeType: 0x4ab0941a | 0x7ec13a06 // classic | advanced
    _terms: [true,true,0,0,0,0,0,0,0]
    ```
    An example contract on Mumbai: https://mumbai.polygonscan.com/address/0x775096a339E54a424EBae52a98cE510FD48C6782
    
    Issue-URL: https://app.clickup.com/t/2hxaf90

--- contracts/deploy/03-Staking.ts
+++ contracts/deploy/03-Staking.ts
@@ -6,15 +6,23 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const {deploy} = deployments;
     const {deployer} = await getNamedAccounts();
 
+    let contract = 'TestnetStaking';
+
+    if (hre.network.name == 'mainnet' || hre.network.name == 'polygon') {
+        contract = 'Staking';
+    }
+
     const master = await hre.ethers.getContract('RewardMaster');
 
     await deploy('Staking', {
+        contract,
         from: deployer,
         args: [process.env.ZKP_TOKEN_ADDRESS, master.address, deployer],
         log: true,
         autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
     });
 };
+
 export default func;
 
 func.tags = ['Staking'];

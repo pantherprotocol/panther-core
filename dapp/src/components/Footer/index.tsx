41ebc4409d5abed106e343c3beeae89f952a1435
Mayas Salman <mayassalman@gmail.com>
2022-08-05 13:34:21 +0300

    dapp: update the footer layout
    this patch update the footer layout to fit the new Figma design for v1.0
    
    Issue-URL: https://app.clickup.com/t/2uv2w7w

--- dapp/src/components/Footer/index.tsx
+++ dapp/src/components/Footer/index.tsx
@@ -10,12 +10,11 @@ import './styles.scss';
 export const Footer = () => {
     return (
         <Grid container className="footer-holder">
-            <Grid item xs={12} md={6} className="footer-links">
-                <Links />
-            </Grid>
-
             <Grid item xs={12} md={6} className="footer-socials">
                 <Socials />
+            </Grid>
+            <Grid item xs={12} md={6} className="footer-links">
+                <Links />
             </Grid>
         </Grid>
     );

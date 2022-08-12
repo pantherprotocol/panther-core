b35c48f9ede32ebb699d93fb3a283a92c75c802c
Mayas Salman <mayassalman@gmail.com>
2022-08-12 14:41:15 +0300

    dapp: change logout title to disconnect
    Issue-URL: https://app.clickup.com/t/389f7ym

--- dapp/src/components/LogoutButton/index.tsx
+++ dapp/src/components/LogoutButton/index.tsx
@@ -24,5 +24,5 @@ export const LogoutButton = () => {
         }
     }, [active, chainId, deactivate, dispatch]);
 
-    return <WalletHeaderActionButton text="Logout" onClick={disconnect} />;
+    return <WalletHeaderActionButton text="Disconnect" onClick={disconnect} />;
 };

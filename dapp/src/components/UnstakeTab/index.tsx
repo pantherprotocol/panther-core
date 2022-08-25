ec471e5026704f9cceecf87a5fc93fd249d68461
Mayas Salman <mayassalman@gmail.com>
2022-08-25 10:38:21 +0300

    dapp: show unstaking tab when disconnected
    Issue-URL: https://app.clickup.com/t/38un714

--- dapp/src/components/UnstakeTab/index.tsx
+++ dapp/src/components/UnstakeTab/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 
-import {Box, Typography} from '@mui/material';
-import {useWeb3React} from '@web3-react/core';
+import {Box} from '@mui/material';
 
 import UnstakeTable from '../UnstakeTable';
 
@@ -10,19 +9,6 @@ import UnstakingInfo from './UnstakingInfo';
 import './styles.scss';
 
 export default function UnstakingTab() {
-    const {active} = useWeb3React();
-
-    if (!active) {
-        return (
-            <Box className="unstaking-tab-holder">
-                <Typography variant="caption">
-                    Please connect your wallet to a supported blockchain to
-                    unstake.
-                </Typography>
-            </Box>
-        );
-    }
-
     return (
         <Box className="unstaking-tab-holder">
             <UnstakingInfo />

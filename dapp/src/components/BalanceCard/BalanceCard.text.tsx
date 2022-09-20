1b149050ef09c1ef1b66fd6ed5af1c6d19983409
Milan Vasic <milan.vasic.su@gmail.com>
2022-09-20 20:08:23 +0200

    dapp: implementation of test case for CardBalance component
    Without this patch, we will not have a basic test case for CardBalance
    component
    
    This patch solves the problem by implementing a proper test case for
    CardBalance component
    
    Issue-URL: https://app.clickup.com/t/2za6mch

---
+++ dapp/src/components/BalanceCard/BalanceCard.text.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import {screen} from '@testing-library/react';
+
+import {renderComponent} from '../../utils/test-utils';
+
+import BalanceCard from '.';
+
+test('should render', () => {
+    renderComponent(<BalanceCard />);
+    const inputWrapper = screen.getByTestId('balance-card');
+    expect(inputWrapper).toBeInTheDocument();
+});

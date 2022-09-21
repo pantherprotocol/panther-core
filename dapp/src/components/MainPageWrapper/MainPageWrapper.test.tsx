ecc33bf061bcdf398fbaf8f46f9868fdbaf1ecbc
Sajjad Heydari <sajjadheydari1401@gmail.com>
2022-09-21 17:41:26 +0430

    dapp: cover MainPageWrapper with proper test
    Without this patch, we don't have test for MainPageWrapper.
    
    This is a problem because tests are required for UI sections.
    
    This patch solves the problem by adding MainPageWrapper.test.tsx and
    changing {...links()} to (links()) in NavigationMenu bcz jest can't
    accept spreading children.
    
    Issue-URL: https://app.clickup.com/t/2za6mf9

---
+++ dapp/src/components/MainPageWrapper/MainPageWrapper.test.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+
+import {screen, waitFor} from '@testing-library/react';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+import {renderComponent} from '../../utils/test-utils';
+
+import {MainPageWrapper} from './index';
+
+test('should render the MainPageWrapper with its child element', () => {
+    const mainPageWrapper = screen.queryByTestId(
+        'main-page-wrapper_main-page-wrapper_container',
+    );
+    const mainPageWrapperChild = screen.queryByTestId(
+        'main-page-wrapper_main-page-wrapper_child',
+    );
+    renderComponent(
+        <Router>
+            <MainPageWrapper>{mainPageWrapperChild}</MainPageWrapper>
+        </Router>,
+    );
+
+    waitFor(() => {
+        expect(mainPageWrapperChild).toBeInTheDocument();
+        expect(mainPageWrapper).toContainElement(mainPageWrapperChild);
+    });
+});

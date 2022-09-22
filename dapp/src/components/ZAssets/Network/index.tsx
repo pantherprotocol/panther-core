176d3618d34031c675fda62bb0c98c7edf6a6c15
Sajjad Heydari <sajjadheydari1401@gmail.com>
2022-09-23 03:16:01 +0330

    dapp: do not reflect network while user not connected / zAssets page
    Without this patch, the Network component shows network name and logo
    while wallet not connected.
    
    This is a problem because it is not a good user experience.
    
    This patch solves the problem by conditionally rendering the network
    info using active property from useWeb3React
    
    Issue-URL: https://app.clickup.com/t/2znmypw

--- dapp/src/components/ZAssets/Network/index.tsx
+++ dapp/src/components/ZAssets/Network/index.tsx
@@ -1,18 +1,29 @@
 import * as React from 'react';
 
 import {Box, Typography} from '@mui/material';
+import {useWeb3React} from '@web3-react/core';
 
 import polygonIcon from '../../../images/polygon-beige-logo.svg';
 
 import './styles.scss';
 
 export default function Network(props: {networkName: string}) {
+    const {active} = useWeb3React();
+
     return (
         <Box className="asset-network">
-            <Typography className="network-logo">
-                <img src={polygonIcon} />
-            </Typography>
-            <Typography className="asset-name">{props.networkName}</Typography>
+            {active ? (
+                <>
+                    <Typography className="network-logo">
+                        <img src={polygonIcon} />
+                    </Typography>
+                    <Typography className="asset-name">
+                        {props.networkName}
+                    </Typography>
+                </>
+            ) : (
+                'Not Connected'
+            )}
         </Box>
     );
 }

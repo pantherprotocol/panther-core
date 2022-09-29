fe16743dfe01f8943023d132ed56bb5cbfe33ec8
sajjad <sajjadheydari1994@gmail.com>
2022-09-29 16:42:16 +0330

    dapp: Implement MessageWithTx storybook
    Without this patch, we lack story book for MessageWithTx component.
    
    This is a problem because because storybook is needed to make UI dev
    process faster.
    
    This patch solves the problem by adding MessageWithTx.stories.tsx &
    MessageWithTx.interface.ts
    
    Issue-URL: https://app.clickup.com/t/306ukau

---
+++ dapp/src/components/Common/MessageWithTx/MessageWithTx.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+// eslint-disable-next-line import/named
+import {Meta, Story} from '@storybook/react';
+import {Provider} from 'react-redux';
+
+import {store} from '../../../redux/store';
+
+import {MessageWithTxProps} from './MessageWithTxProps.interface';
+
+import {MessageWithTx} from './index';
+
+export default {
+    title: 'MessageWithTx',
+    component: MessageWithTx,
+} as Meta;
+
+const Template: Story<MessageWithTxProps> = args => (
+    <Provider store={store}>
+        <MessageWithTx {...args} />
+    </Provider>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+    message: 'message',
+    chainId: 80001,
+    linkText: 'link text',
+    txHash: '0x0f721f219d7cbac5ffc605a4dd5b017a7b3813d9686a1cfe75fa712ddbd2ca78',
+};

1e1805a7ea4bf486a0a6c80a05f6d64f1220ff18
sajjad <sajjadheydari1994@gmail.com>
2022-09-29 17:33:44 +0330

    dapp: Implement BackButton storybook
    Without this patch, we lack story book for BackButton component.
    
    This is a problem because because storybook is needed to make UI dev
    process faster.
    
    This patch solves the problem by adding BackButton.stories.tsx &
    BackButton.interface.ts
    
    Issue-URL: https://app.clickup.com/t/306ukff

---
+++ dapp/src/components/BackButton/BackButton.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+// eslint-disable-next-line import/named
+import {Meta, Story} from '@storybook/react';
+import {Provider} from 'react-redux';
+
+import {store} from '../../redux/store';
+
+import {BackButtonProps} from './BackButton.interface';
+
+import BackButton from './index';
+
+export default {
+    title: 'BackButton',
+    component: BackButton,
+    argTypes: {
+        argTypes: {onClick: {action: 'clicked'}},
+    },
+} as Meta;
+
+const Template: Story<BackButtonProps> = args => (
+    <Provider store={store}>
+        <BackButton {...args} />
+    </Provider>
+);
+
+export const Default = Template.bind({});
+
+Default.args = {
+    onClick: () => console.log('Button Clicked!'),
+};

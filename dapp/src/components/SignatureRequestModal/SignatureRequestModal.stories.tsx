06cbd5cc557b4186a7218225175c2b586048b0e8
sajjad <sajjadheydari1994@gmail.com>
2022-10-04 15:44:56 +0330

    dapp: implement SignatureRequestModal storybook
    Without this patch, we lack storybook for SignatureRequestModal component.
    
    This is a problem because because storybook is needed to make UI dev process
    faster.
    
    This patch solves the problem by adding SignatureRequestModal.stories.tsx.
    
    Issue-URL: https://app.clickup.com/t/306ukut

---
+++ dapp/src/components/SignatureRequestModal/SignatureRequestModal.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// eslint-disable-next-line import/named
+import {Meta, Story} from '@storybook/react';
+import {Provider} from 'react-redux';
+
+import {store} from '../../redux/store';
+
+import SignatureRequestModal from './index';
+
+export default {
+    title: 'SignatureRequestModal',
+    component: SignatureRequestModal,
+} as Meta;
+
+const Template: Story = () => (
+    <Provider store={store}>
+        <SignatureRequestModal />
+    </Provider>
+);
+
+export const Default = Template.bind({});

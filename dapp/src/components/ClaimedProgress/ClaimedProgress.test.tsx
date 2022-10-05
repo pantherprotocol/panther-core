ce079ba81e1303ab40abf5d79023f06555dcf241
sajjad <sajjadheydari1994@gmail.com>
2022-10-05 22:09:41 +0330

    dapp: cover ClaimedProgress with proper test
    Without this patch, we do not have test cases for ClaimedProgress component.
    
    This is a problem because tests are vital for UI sections.
    
    This patch solves the problem by adding ClaimedProgress.test.tsx file.
    
    Issue-URL: https://app.clickup.com/t/2za6mdj

---
+++ dapp/src/components/ClaimedProgress/ClaimedProgress.test.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+
+import {screen, waitFor} from '@testing-library/react';
+
+import {renderComponent} from '../../utils/test-utils';
+
+import ClaimedProgress from './index';
+
+test('should render', async () => {
+    const testTotal = 1000;
+    const testClaimed = 100;
+
+    renderComponent(
+        <ClaimedProgress total={testTotal} claimed={testClaimed} />,
+    );
+    const claimedProgressContainer = screen.queryByTestId(
+        'claimed-progress_claimed-progress_container',
+    );
+    await waitFor(() => expect(claimedProgressContainer).toBeInTheDocument());
+});
+
+test('should format numbers correctly', async () => {
+    const testTotal = 10000000;
+    const testClaimed = 10000;
+
+    renderComponent(
+        <ClaimedProgress total={testTotal} claimed={testClaimed} />,
+    );
+
+    const claimedProgressContainer = screen.queryByTestId(
+        'claimed-progress_claimed-progress_container',
+    );
+
+    const claimedProgressValueElement = screen.queryByTestId(
+        'claimed-progress_claimed-progress_value',
+    );
+
+    await waitFor(() => {
+        expect(claimedProgressContainer).toBeInTheDocument(),
+            expect(claimedProgressValueElement).toBeInTheDocument();
+    });
+
+    const claimedProgressValue = await claimedProgressValueElement?.innerHTML;
+
+    await waitFor(() => {
+        expect(claimedProgressValue?.includes('10k out of 10m')).toBeTruthy();
+    });
+});

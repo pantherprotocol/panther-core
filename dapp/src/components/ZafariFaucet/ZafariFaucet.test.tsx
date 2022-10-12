32489cdfccc9e57e782ae5fd8db4e4df43a667e3
sajjad <sajjadheydari1994@gmail.com>
2022-10-12 23:52:00 +0330

    dapp: cover ZafariFaucet with proper test
    Without this patch, we do not have test cases for ZafariFaucet component.
    
    This is a problem because tests are vital for UI sections.
    
    This patch solves the problem by adding ZafariFaucet.test.tsx file.
    
    Issue-URL: https://app.clickup.com/t/2za6ncg

---
+++ dapp/src/components/ZafariFaucet/ZafariFaucet.test.tsx
@@ -0,0 +1,83 @@
+import * as React from 'react';
+
+import {fireEvent, screen, waitFor} from '@testing-library/react';
+import {Web3ReactProvider} from '@web3-react/core';
+
+import {getLibrary} from '../../services/provider';
+import {renderComponent} from '../../utils/test-utils';
+
+import ZafariFaucet from './index';
+
+test('should render with correct texts when wallet is disconnected', async () => {
+    renderComponent(<ZafariFaucet />);
+
+    const zafariFaucetContainer = screen.queryByTestId(
+        'zafari-faucet_zafari-faucet_container',
+    );
+
+    const zafariFaucetNetwork = screen.queryByTestId(
+        'zafari-faucet_zafari-faucet_network',
+    );
+
+    const zafariFaucetAddress = screen.queryByTestId(
+        'zafari-faucet_zafari-faucet_address',
+    );
+
+    const zafariFaucetButtonHolder = screen.queryByTestId(
+        'zafari-faucet_zafari-faucet_button-holder',
+    );
+
+    await waitFor(() => {
+        expect(zafariFaucetContainer).toBeInTheDocument();
+        expect(zafariFaucetNetwork).toBeInTheDocument();
+        expect(zafariFaucetAddress).toBeInTheDocument();
+        expect(zafariFaucetButtonHolder).toBeInTheDocument();
+    });
+
+    await waitFor(() => {
+        expect(zafariFaucetNetwork?.textContent).toBe('Wallet not connected');
+        expect(zafariFaucetAddress?.textContent).toBe('Wallet not connected');
+        expect(zafariFaucetButtonHolder?.textContent).toBe('Connect Wallet');
+    });
+});
+
+test('click on button should trigger onClick event', async () => {
+    renderComponent(
+        <Web3ReactProvider getLibrary={getLibrary}>
+            <ZafariFaucet />
+        </Web3ReactProvider>,
+    );
+
+    const zafariFaucetNetwork = screen.queryByTestId(
+        'zafari-faucet_zafari-faucet_network',
+    );
+
+    const zafariFaucetAddress = screen.queryByTestId(
+        'zafari-faucet_zafari-faucet_address',
+    );
+
+    const zafariFaucetButtonHolder = screen.queryByTestId(
+        'zafari-faucet_zafari-faucet_button-holder',
+    );
+
+    const zafariFaucetConnectButton = screen.queryByRole('button', {
+        name: /Connect Wallet/i,
+    });
+
+    await waitFor(() => {
+        expect(zafariFaucetNetwork).toBeInTheDocument();
+        expect(zafariFaucetAddress).toBeInTheDocument();
+        expect(zafariFaucetButtonHolder).toBeInTheDocument();
+        expect(zafariFaucetConnectButton).toBeInTheDocument();
+    });
+
+    await waitFor(
+        () =>
+            zafariFaucetConnectButton &&
+            fireEvent.click(zafariFaucetConnectButton),
+    );
+
+    await waitFor(() => {
+        expect(zafariFaucetButtonHolder?.textContent).toBe('Install MetaMask');
+    });
+});

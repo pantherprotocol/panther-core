9f718ef027e9ccca4dd673ab63174b43ae7d4a9c
Sajjad <sajjadheydari1401@gmail.com>
2022-10-19 21:22:13 +0330

    dapp: Fix copy address button active state
    Without this patch, user can not know if address is coppied when
    clicked on the copy icon in balance card.
    
    This is a problem because it has not a good UX.
    
    This patch solves the problem by setting active class to the element
    when the icon is clicked.
    
    Issue-URL: https://app.clickup.com/t/3q30mge

--- dapp/src/components/BalanceCard/AddressWithSetting/index.tsx
+++ dapp/src/components/BalanceCard/AddressWithSetting/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 
+import CheckOutlinedIcon from '@mui/icons-material/CheckOutlined';
 import {Box, Tooltip} from '@mui/material';
 import CopyToClipboard from 'react-copy-to-clipboard';
 
@@ -9,15 +10,31 @@ import Address from '../../Address';
 import './styles.scss';
 
 export default function AddressWithSetting(props: {account: string}) {
+    const [copied, setCopied] = useState(false);
+
+    const handleCopy = () => {
+        setCopied(true);
+        setTimeout(() => {
+            setCopied(false);
+        }, 2000);
+    };
+
     return (
         <Box className="address-with-setting">
             <Box className="address">
                 <Address />
             </Box>
-            <Tooltip title="Copy Wallet Address" placement="top">
-                <span className="copy-icon-wrapper">
-                    <CopyToClipboard text={props.account}>
-                        <img src={copyIcon} alt="copy-icon" />
+            <Tooltip
+                title={`${copied ? 'Copied!' : 'Copy Wallet Address'}`}
+                placement="top"
+            >
+                <span className={`copy-icon-wrapper ${copied && 'copied'}`}>
+                    <CopyToClipboard text={props.account} onCopy={handleCopy}>
+                        {copied ? (
+                            <CheckOutlinedIcon />
+                        ) : (
+                            <img src={copyIcon} alt="copy-icon" />
+                        )}
                     </CopyToClipboard>
                 </span>
             </Tooltip>

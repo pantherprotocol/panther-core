741d028c4592cfec962c3fff1e5412beba6b9b5b
Sajjad <sajjadheydari1401@gmail.com>
2022-10-31 23:11:46 +0330

    dapp: fix footer to match figma
    Without this patch, we have not followed figma
    for footer latest design.
    
    This is a problem because we should shall follow
    figma design.
    
    This patch solves the problem by applying needed
    styles to related files accordingly.
    
    Issue-URL: https://app.clickup.com/t/3r5ezp2

--- dapp/src/components/Footer/Links/index.tsx
+++ dapp/src/components/Footer/Links/index.tsx
@@ -2,11 +2,12 @@ import React, {useState} from 'react';
 
 import {SafeLink} from 'components/Common/links';
 import UserTerms from 'components/ScrollableDialog';
+import useScreenSize from 'hooks/screen';
 import {Link} from 'react-router-dom';
 
 import './styles.scss';
 
-type NormalLink = {
+type Hyperlink = {
     text: string;
     href: string;
 };
@@ -14,15 +15,18 @@ type ActionLink = {
     text: string;
     onclick: (e: any) => void;
 };
+type RouteLink = {
+    name: string;
+    to: string;
+};
 
-type LinkType = NormalLink | ActionLink;
-
-function isNormalLink(link: LinkType): link is NormalLink {
-    return 'href' in link;
-}
+type LinkType = Hyperlink | ActionLink;
+type RouteType = Hyperlink | RouteLink;
 
 const Links = () => {
+    const {isMobile} = useScreenSize();
     const [showUserTermsDialog, setShowUserTermsDialog] = useState(false);
+
     const handleCloseWarningDialog = () => {
         setShowUserTermsDialog(false);
     };
@@ -45,26 +49,60 @@ const Links = () => {
             href: 'https://www.pantherprotocol.io/privacy-policy',
         },
     ];
+
+    const routesData: RouteType[] = [
+        {
+            name: 'Staking',
+            to: '/',
+        },
+        {
+            name: 'zAssets',
+            to: '/zAssets',
+        },
+        {
+            text: 'Governance',
+            href: 'https://snapshot.org/#/pantherprotocol.eth',
+        },
+    ];
+
+    const createLink = (linkObj: LinkType | RouteType) => {
+        if ('href' in linkObj) {
+            return <SafeLink href={linkObj.href}>{linkObj.text}</SafeLink>;
+        } else {
+            if ('onclick' in linkObj) {
+                return (
+                    <Link
+                        to="#"
+                        onClick={linkObj.onclick}
+                        data-testid={linkObj.text}
+                    >
+                        {linkObj.text}
+                    </Link>
+                );
+            } else {
+                return <Link to={linkObj.to}>{linkObj.name}</Link>;
+            }
+        }
+    };
+
     return (
         <div className="adv-staking-footer-links">
-            {linksData.map((link: LinkType, index) => (
-                <div key={index}>
-                    {isNormalLink(link) ? (
-                        <SafeLink href={link.href}>{link.text}</SafeLink>
-                    ) : (
-                        <Link
-                            to="#"
-                            onClick={link.onclick}
-                            data-testid={link.text}
-                        >
-                            {link.text}
-                        </Link>
-                    )}
+            {!isMobile &&
+                linksData.map((link: LinkType, index) => (
+                    <div key={index}>
+                        {createLink(link)}
+                        <span className="separator">
+                            {linksData.length !== index + 1 ? '|' : ''}
+                        </span>
+                    </div>
+                ))}
 
-                    {linksData.length !== index + 1 ? '|' : ''}
-                </div>
-            ))}
-
+            {isMobile &&
+                routesData.map((route: RouteType, index) => (
+                    <div key={index} className="footer-route">
+                        {createLink(route)}
+                    </div>
+                ))}
             {showUserTermsDialog && (
                 <UserTerms
                     handleClose={handleCloseWarningDialog}

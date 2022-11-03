3eb819ec38aa19889651d2eda1bb9e1ee4d53dab
Sajjad <sajjadheydari1401@gmail.com>
2022-11-03 21:27:23 +0330

    dapp: cover AdvancedStakingRewards with proper test
    Without this patch, we do not have test cases for
    AdvancedStakingRewards component.
    
    This is a problem because tests are vital for UI sections.
    
    This patch solves the problem by adding AdvancedStakingRewards.test.tsx file.
    
    Issue-URL: https://app.clickup.com/t/2za624y

---
+++ dapp/src/components/AdvancedStakingRewards/AdvancedStakingRewards.test.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+
+import {screen, waitFor} from '@testing-library/react';
+
+import {renderComponent} from '../../utils/test-utils';
+
+import AdvancedStakingRewards, {StakingAPR} from './index';
+
+test('should render', async () => {
+    renderComponent(<AdvancedStakingRewards />);
+
+    const advancedStakingRewardsContainer = screen.queryByTestId(
+        'advanced-staking-rewards_advanced-staking-rewards_container',
+    );
+    const remainingDaysContainer = screen.queryByTestId(
+        'advanced-staking-rewards_remaining-days_container',
+    );
+
+    await waitFor(() => {
+        expect(advancedStakingRewardsContainer).toBeInTheDocument();
+        expect(remainingDaysContainer).toBeInTheDocument();
+    });
+});
+
+test('should format APR correctly', async () => {
+    const testAPY = 40;
+
+    renderComponent(<StakingAPR advancedStakingAPY={testAPY} />);
+
+    const stakingAprContainer = screen.queryByTestId(
+        'advanced-staking-rewards_staking-apr_container',
+    );
+
+    const stakingAprValueElement = screen.queryByTestId(
+        'advanced-staking-rewards_staking-apr_value',
+    );
+
+    await waitFor(() => {
+        expect(stakingAprContainer).toBeInTheDocument();
+        expect(stakingAprValueElement).toBeInTheDocument();
+    });
+
+    const stakingAprValue = await stakingAprValueElement?.innerHTML;
+
+    await waitFor(() => {
+        expect(stakingAprValue).toBe('40.00%');
+    });
+});

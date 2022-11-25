2e2fbadc521034bc1787f4e6e93538d3f3385307
pycckuu <is.markelov@gmail.com>
2022-11-25 16:24:58 +0100

    dapp: preparing .env file for production deployment
    Without this patch, we do not have the .env file for the production
    deployment. This patch fixes that.

--- dapp/scripts/deploy-ipfs.sh
+++ dapp/scripts/deploy-ipfs.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
 
-# This script deploys dApp to the IPFS. It will be available after PIP-9 is
-# executed.
+rm -rfd build
+NODE_ENV=production yarn build:prod
+ipfs add -r build --quiet | tail -n 1 > ipfs_hash.txt
+ipfs pin add -r $(cat ipfs_hash.txt)
+echo assets build pinned to https://ipfs.io/ipfs/$(cat ipfs_hash.txt)
+
+# # replase substrings in index.html file
+filename="build/index.html"
+
+search="bundle.js"
+replace="https:\/\/ipfs.io\/ipfs\/$(cat ipfs_hash.txt)\/bundle.js"
+sed -i '' "s/$search/$replace/" $filename
+
+search="%PUBLIC_URL%\/"
+replace=""
+sed -i '' "s/$search/$replace/" $filename
+
+search="logo.png"
+replace="https:\/\/ipfs.io\/ipfs\/$(cat ipfs_hash.txt)\/logo.png"
+sed -i '' "s/$search/$replace/g" $filename
+
+search="\/manifest"
+replace="https:\/\/ipfs.io\/ipfs\/$(cat ipfs_hash.txt)\/manifest"
+sed -i '' "s/$search/$replace/g" $filename
+
+ipfs add -r build --quiet | tail -n 1 > ipfs_hash.txt
+ipfs pin add -r $(cat ipfs_hash.txt)
+echo App deployed to https://ipfs.io/ipfs/$(cat ipfs_hash.txt)
+
+# echo publishing to IPNS -> $(cat ipfs_hash.txt)
+# ipfs name publish $(cat ipfs_hash.txt)
+
+# cleaning
+rm ipfs_hash.txt

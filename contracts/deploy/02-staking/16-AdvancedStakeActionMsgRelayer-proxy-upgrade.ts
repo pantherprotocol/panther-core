9975369ffdfdc2b4dee2be66d3627aae99ccb749
Behzad <whoisbehzad@gmail.com>
2022-11-27 19:27:50 +0330

    contracts: use helper function to upgrade relayer proxy
    This patch solves the problem by using `upgradeEIP1967Proxy` helper
    function to update the proxt to the implementation.

--- contracts/deploy/02-staking/16-AdvancedStakeActionMsgRelayer-proxy-upgrade.ts
+++ contracts/deploy/02-staking/16-AdvancedStakeActionMsgRelayer-proxy-upgrade.ts
@@ -2,6 +2,10 @@ import {DeployFunction} from 'hardhat-deploy/types';
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 
 import {isPolygonOrMumbai} from '../../lib/checkNetwork';
+import {
+    getContractAddress,
+    upgradeEIP1967Proxy,
+} from '../../lib/deploymentHelpers';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     if (
@@ -12,50 +16,27 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
         return;
     }
 
-    const {getNamedAccounts, ethers} = hre;
+    const {getNamedAccounts} = hre;
     const {deployer} = await getNamedAccounts();
 
-    let relayerProxy, relayerImp;
-
-    try {
-        relayerProxy = await ethers.getContract(
-            'AdvancedStakeActionMsgRelayer_Proxy',
-        );
-        relayerImp = await ethers.getContract(
-            'AdvancedStakeActionMsgRelayer_Implementation',
-        );
-    } catch (error: any) {
-        console.log('skip proxy upgrade:', error.message);
-
-        return;
-    }
-
-    // `.send` used instead of `.getStorageAt` to work w/ both `ganache-cli` and `hardhat`
-    // (`.getStorageAt` fails on '0x', which `ganache-cli` returns if the slot is empty)
-    const response = await ethers.provider.send('eth_getStorageAt', [
-        relayerProxy.address,
-        // EIP-1967 implementation slot
-        '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc',
-    ]);
-    const oldImpl: string = ethers.utils.hexZeroPad(
-        ethers.utils.hexStripZeros(response),
-        20,
+    const relayerProxy = await getContractAddress(
+        hre,
+        'AdvancedStakeActionMsgRelayer_Proxy',
+        'ADVANCED_STAKE_ACTION_MSG_RELAYER_PROXY',
     );
-    if (oldImpl == relayerImp.address.toLowerCase()) {
-        console.log(
-            `Relayer_Proxy already set to Relayer_Implementation: ${relayerImp.address}`,
-        );
-    } else {
-        console.log(
-            `Upgrading Relayer_Proxy to new Relayer_Implementation: ${relayerImp.address}...`,
-        );
+    const relayerImp = await getContractAddress(
+        hre,
+        'AdvancedStakeActionMsgRelayer_Implementation',
+        'ADVANCED_STAKE_ACTION_MSG_RELAYER_PROXY_IMP',
+    );
 
-        const signer = await ethers.getSigner(deployer);
-        const tx = await relayerProxy
-            .connect(signer)
-            .upgradeTo(relayerImp.address);
-        console.log('Relayer_Proxy is upgraded, tx: ', tx.hash);
-    }
+    await upgradeEIP1967Proxy(
+        hre,
+        deployer,
+        relayerProxy,
+        relayerImp,
+        'AdvancedStakeActionMsgRelayer',
+    );
 };
 
 export default func;

1f47ab34dfa79f5f1abe4e0044c2d1ebb8df4f8e
pycckuu <is.markelov@gmail.com>
2022-12-08 18:14:33 +0000

    dapp: removing the terms link in the popup

--- dapp/src/components/BlockedUser/index.tsx
+++ dapp/src/components/BlockedUser/index.tsx
@@ -12,11 +12,8 @@ import {
     Typography,
     IconButton,
 } from '@mui/material';
-import {SafeMuiLink} from 'components/Common/links';
-import PrimaryActionButton from 'components/Common/PrimaryActionButton';
 import UserTerms from 'components/ScrollableDialog';
 import warningIcon from 'images/warning-icon.svg';
-import {Link} from 'react-router-dom';
 
 import './styles.scss';
 
@@ -24,10 +21,6 @@ export default function BlockedUser(): ReactElement {
     const [showUserTermsDialog, setShowUserTermsDialog] = useState(false);
     const handleCloseWarningDialog = () => {
         setShowUserTermsDialog(false);
-    };
-
-    const handleOpenWarningDialog = () => {
-        setShowUserTermsDialog(true);
     };
 
     return showUserTermsDialog ? (
@@ -51,33 +44,10 @@ export default function BlockedUser(): ReactElement {
                     <Box>
                         It appears that you are visiting from the prohibited
                         jurisdiction. Unfortunately, Panther's Advanced Staking
-                        is not available in this area. You can read about this{' '}
-                        <Link to="#" onClick={handleOpenWarningDialog}>
-                            here
-                        </Link>
-                        .
-                        <br />
-                        <br />
-                        If you would like to explore and test the Panther dApp,
-                        you can continue to Panther's Testing Environment. To
-                        learn more about the Panther Protocol, visit our{' '}
-                        <SafeMuiLink
-                            href="http://pantherprotocol.io/"
-                            underline="always"
-                        >
-                            Website
-                        </SafeMuiLink>{' '}
+                        is not available in this area.
                     </Box>
                 </Box>
             </DialogContent>
-
-            <SafeMuiLink href="https://test.staking.pantherprotocol.io/">
-                <PrimaryActionButton>
-                    <Typography>
-                        <strong>Continue</strong>
-                    </Typography>
-                </PrimaryActionButton>
-            </SafeMuiLink>
         </Dialog>
     );
 }

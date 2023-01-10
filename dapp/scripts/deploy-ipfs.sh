c896a30b2825b33e785968dbc04b3c1dc71a2fa8
Milan Vasic <milan.vasic.su@gmail.com>
2023-01-10 14:50:32 +0100

    feat(dapp): improved way of ipfs deployment to be OS agnostic
    Without this patch, users which are on Linux based systems will have problem to deploy on IPFS, since sed function do not require additional parameter
    
    This is a problem because sed function is different on mac OS based system than on Unix based systems with set of inline parameters
    
    This patch solves the problem by adding a case to check which OS system is user running and based on it we do proper call of sed function with list of parameters
    
    Issue-URL: https://app.clickup.com/t/861m6u7vk

--- dapp/scripts/deploy-ipfs.sh
+++ dapp/scripts/deploy-ipfs.sh
@@ -6,6 +6,14 @@ ipfs add -r build --quiet | tail -n 1 > ipfs_hash.txt
 ipfs pin add -r $(cat ipfs_hash.txt)
 echo assets build pinned to https://ipfs.io/ipfs/$(cat ipfs_hash.txt)
 
+function replaceFileNames() {
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -i '' "$1" "$2"
+    else
+        sed -i "$1" "$2"
+    fi
+}
+
 # # replase substrings in index.html file
 filename="build/index.html"
 
@@ -15,15 +23,15 @@ sed -i '' "s/$search/$replace/" $filename
 
 search="%PUBLIC_URL%\/"
 replace=""
-sed -i '' "s/$search/$replace/" $filename
+replaceFileNames "s/$search/$replace/" $filename
 
 search="logo.png"
 replace="https:\/\/ipfs.io\/ipfs\/$(cat ipfs_hash.txt)\/logo.png"
-sed -i '' "s/$search/$replace/g" $filename
+replaceFileNames "s/$search/$replace/g" $filename
 
 search="\/manifest"
 replace="https:\/\/ipfs.io\/ipfs\/$(cat ipfs_hash.txt)\/manifest"
-sed -i '' "s/$search/$replace/g" $filename
+replaceFileNames "s/$search/$replace/g" $filename
 
 ipfs add -r build --quiet | tail -n 1 > ipfs_hash.txt
 ipfs pin add -r $(cat ipfs_hash.txt)

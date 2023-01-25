0fc77eef663dbba6565bd98199e67eb239472728
pycckuu <is.markelov@gmail.com>
2023-01-25 15:58:51 +0800

    feat(dapp): add env variables for staging and production
    Without this patch, we cannot conditionally build and render the app for
    staging and production.
    
    This is a problem because we need to add the testing background image
    for staging, and, most likely, in the future, we will need to have some
    other distinct features for staging and production.
    
    This patch solves the problem by adding the
    TARGET_ENV={development,staging,production} env variable. As a result,
    this patch updates the webpack config to build the app for staging and
    production conditionally: more specifically, it conditionally adds the
    source map files, optimization of the js files, and Sentry build upload.
    Moreover, the patch updates package.json, the Amplify and GitLab scripts
    to use the new env variable. Also, the patch adds an env.staging file
    for test staging deployment and updates the env example files. Finally,
    the patch updates the GitLab CI script, removing the unnecessary second
    run of the dapp test.

--- dapp/src/services/env.ts
+++ dapp/src/services/env.ts
@@ -7,6 +7,7 @@
 //////////////////////////////////////////////////////////////////////////////
 
 const REQUIRED_ENV_VARS: string[] = [
+    'TARGET_ENV',
     'APP_MODE',
     'CHAIN_IDS',
     'MASP_CHAIN_ID',
@@ -35,6 +36,7 @@ interface EnvVariables {
 }
 
 export const env: EnvVariables = {
+    TARGET_ENV: process.env.TARGET_ENV,
     APP_MODE: process.env.APP_MODE,
     FAUCET_BASE_URL: process.env.FAUCET_BASE_URL,
     CHAIN_IDS: process.env.CHAIN_IDS,
@@ -176,7 +178,6 @@ export function getMissingEnvVars(): string[] {
         .sort();
 }
 
-export function isStaging() {
-    const NODE_ENV = process.env.NODE_ENV as string;
-    return NODE_ENV === 'staging';
+export function isStaging(): boolean {
+    return env.TARGET_ENV === 'staging';
 }

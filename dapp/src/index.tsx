b28e7868bc28a0439aad30416713c6268ee99f80
Ahmed Ibrahim <ib153507@gmail.com>
2023-02-01 11:54:55 +0200

    fix(dapp): review eslint warning
    Without this patch, an eslint warning will be shown when running `yarn lint:ts`
    
    This is a problem because the warning says that the current version of eslint
    doesn't support typescript 4.9.4 that we just updated it recently.
    
    This patch solves the problem by upgrading all `eslint` related packages into
    the latest version and resolving all the errors caused by the new version.
    
    Issue-URL: https://app.clickup.com/t/86779bvz1

--- dapp/src/index.tsx
+++ dapp/src/index.tsx
@@ -9,8 +9,8 @@ import {BrowserTracing} from '@sentry/tracing';
 import {Web3ReactProvider} from '@web3-react/core';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {PersistGate} from 'redux-persist/integration/react';
 import {persistor, store} from 'redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
 import {getLibrary} from 'services/provider';
 
 import App from './App';

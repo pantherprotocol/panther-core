60344fbd5aa7f5390f0bdeaa25a94db59569d55b
Mayas <mayassalman@gmail.com>
2023-02-20 09:48:33 +0300

    refactor(dapp): replace date-fns with dayjs
    Without this patch, we currently use `date-fns` library parses,
    manipulates, and displays dates.
    
    This is a problem because it add some extra unnecessary size to the
    final bundle.
    
    This patch solves the problem by replacing `date-fns` with the `dayjs`.
    
    Issue-URL: https://app.clickup.com/t/86699e24b

--- dapp/tests/lib/format.test.ts
+++ dapp/tests/lib/format.test.ts
@@ -2,27 +2,27 @@
 // SPDX-FileCopyrightText: Copyright 2021-22 Panther Ventures Limited Gibraltar
 
 import {describe, expect} from '@jest/globals';
-import {addDays, addHours, subDays} from 'date-fns';
+import dayjs, {Dayjs} from 'dayjs';
 import {formatRemainingPeriod} from 'lib/format';
 
 describe('Checking Dates', () => {
-    const currentTime = new Date();
+    const currentTime = dayjs();
 
     const TEST_CASES = [
-        [subDays(currentTime, 1), '-', 'before current time'],
-        [addHours(currentTime, 23), '23 hours', 'after less than one day'],
-        [addHours(currentTime, 25), '2 days', 'after more than one day'],
-        [addHours(currentTime, 48), '2 days', 'after exactly two days'],
-        [addHours(currentTime, 49), '3 days', 'after more than two days'],
-        [addDays(currentTime, 90), '90 days', 'after 90 day'],
-        [addDays(currentTime, 150), '150 days', 'after 150 day'],
-        [addDays(currentTime, 180), '180 days', 'after 180 day'],
+        [currentTime.subtract(1, 'day'), '-', 'before current time'],
+        [currentTime.add(23, 'hour'), '23 hours', 'after less than one day'],
+        [currentTime.add(25, 'hour'), '2 days', 'after more than one day'],
+        [currentTime.add(48, 'hour'), '2 days', 'after exactly two days'],
+        [currentTime.add(49, 'hour'), '3 days', 'after more than two days'],
+        [currentTime.add(90, 'day'), '90 days', 'after 90 day'],
+        [currentTime.add(150, 'day'), '150 days', 'after 150 day'],
+        [currentTime.add(180, 'day'), '180 days', 'after 180 day'],
     ];
 
     describe('Formatted remaining period:', () => {
         for (const [input, expected, title] of TEST_CASES) {
             it(`should be (${expected}) for (${title})`, () => {
-                expect(formatRemainingPeriod(input as Date)).toEqual(expected);
+                expect(formatRemainingPeriod(input as Dayjs)).toEqual(expected);
             });
         }
     });
